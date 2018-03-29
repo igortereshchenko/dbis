1d6d30c2665d5a310b14e19e62173cfde9e1e83a
Григорий <loco_inverso@ukr.net>
2018-03-29 22:20:09 +0300

    Update laboratory1.sql

--- km62/Dmytrenko_Hryhorii/laboratory1.sql
+++ km62/Dmytrenko_Hryhorii/laboratory1.sql
@@ -9,14 +9,14 @@
 --Код відповідь:
 CREATE USER dmytrenko IDENTIFIED BY password
 DEFAULT TABLESPACE "USERS"
-TEMPORARY TABLESPACE "TABLE"
+TEMPORARY TABLESPACE "TABLE";
 
 GRANT "CONNECT" TO dmytrenko
 
-ALTER USER dmytrenko ON QUOTE 100M
+ALTER USER dmytrenko QUOTA 100M ON USERS;
 
-GRANT DROP ANY TABLE TO dmytrenko
-GRANT INSERT ANY TABLE TO dmytrenko
+GRANT DROP ANY TABLE TO dmytrenko;
+GRANT INSERT ANY TABLE TO dmytrenko;
 
 /*---------------------------------------------------------------------------
 2. Створити таблиці, у яких визначити поля та типи. Головні та зовнішні ключі 
@@ -39,22 +39,22 @@ CREATE TABLE PHONE_TYPE(
 );
 
 ALTER TABLE PHONE_TYPE
-    ADD CONSTAINT phone_type_name_pk PRIMARY KEY (phone_type_name);
+    ADD CONSTRAINT phone_type_name_pk PRIMARY KEY (phone_type_name);
     
 CREATE TABLE HUMAN_PHONE(
-    human_name VARCHAR(20) NOT NULL
-    phone_type_name VARCHAR(20) NOT NULL
+    human_name_fk VARCHAR(20) NOT NULL,
+    phone_type_name_fk VARCHAR(20) NOT NULL,
     human_own_phone VARCHAR(20) NOT NULL
 );
 
 ALTER TABLE HUMAN_PHONE
-    ADD CONSTRAINT human_own_phone_pk PRIMARY KEY (human_name, phone_type_name);
+    ADD CONSTRAINT human_own_phone_pk PRIMARY KEY (human_name_fk, phone_type_name_fk);
     
 ALTER TABLE HUMAN_PHONE
-    ADD CONSTRAINT human_name_fk FOREIGN KEY REFERENCES TO (human_name);
+    ADD CONSTRAINT human_fk FOREIGN KEY (human_name_fk) REFERENCES HUMAN (human_name);
     
-ALTER TABLE HUMAN_TABLE
-    ADD CONSTRAINT phone_type_name_fk FOREIGN KEY REFERENCES TO (phone_type_name);
+ALTER TABLE HUMAN_PHONE
+    ADD CONSTRAINT phone_fk FOREIGN KEY (phone_type_name_fk) REFERENCES PHONE_TYPE (phone_type_name);
 /* --------------------------------------------------------------------------- 
 3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
 внесення даних у таблиці та виконання вибірок використовуючи команду ALTER/GRANT. 
@@ -64,7 +64,7 @@ ALTER TABLE HUMAN_TABLE
 --Код відповідь:
 
 GRANT CREATE ANY TABLE TO dmytrenko
-GRANT INSERT ANY TABLE TO dmytrenko
+GRANT ALTER ANY TABLE TO dmytrenko
 GRANT SELECT ANY TABLE TO dmytrenko;
 
 /*---------------------------------------------------------------------------
@@ -75,19 +75,7 @@ GRANT SELECT ANY TABLE TO dmytrenko;
 ---------------------------------------------------------------------------*/
 
 --Код відповідь:
-
-
-
-
-
-
-
-
-
-
-
-
-
+PROJECT PRODUCTS WHERE PROD_ID IN(PROJECT PRODUCTS{PROD_ID} MINUS PROFECT ORDERITEMS{PROD_ID}) {MAX(PROD_PRICE)}}
 
 /*---------------------------------------------------------------------------
 3.b. 
@@ -103,7 +91,7 @@ SELECT CUST_NAME AS Customer_name
 FROM CUSTOMERS, ORDERITEMS, ORDERS
     WHERE ORDERS.CUST_ID = CUSTOMERS.CUST_ID
     AND ORDERITEMS.ORDER_NUM = ORDERS.ORDER_NUM
-    
+    AND ORDERITEMS.ITEM_PRICE in (Select max(ITEM_PRICE) From ORDERITEMS);
 
 /*---------------------------------------------------------------------------
 c. 
@@ -113,4 +101,4 @@ c.
 
 ---------------------------------------------------------------------------*/
 --Код відповідь:
-
+SELECT vend_name || ' ' || vend_country as vendor_name FROM Vendors WHERE vend_id in (SELECT vend_id FROM Vendors MINUS SELECT vend_id FROM Products )
