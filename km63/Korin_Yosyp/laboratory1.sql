c3b9928bd97a3e07ef6e3d9a124321e93e718443
yKorin <korinyosyp@ukr.net>
2018-03-30 19:21:23 +0300

    Update laboratory1.sql

--- km63/Korin_Yosyp/laboratory1.sql
+++ km63/Korin_Yosyp/laboratory1.sql
@@ -1,6 +1,4 @@
--- LABORATORY WORK 1
--- BY Korin_Yosyp
-/*---------------------------------------------------------------------------
+-----------------------------------------------------
 1.Створити схему даних з назвою – прізвище студента, у якій користувач може: 
 змінювати структуру таблиць та видаляти таблиці
 4 бали
@@ -8,14 +6,14 @@
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 CREATE USER korin IDENTIFIED  BY korin
-DEFAULT TABLESPACE 'USER'
-TEMPORARY TABLESPACE 'TREMP';
+DEFAULT TABLESPACE "USER";
+TEMPORARY TABLESPACE "TREMP";
 
-ALTER TABLE korin QUOTA 100 ON USER;
+ALTER TABLE korin QUOTA 100M ON USER;
 
-GRANT 'CONNECT' ANY TABLE TO korin;
+GRANT 'CONNECT' TO korin;
 
-GRANT UPDATE ANY TABLE TO korin;
+GRANT ALTER ANY TABLE TO korin;
 GRANT DELETE ANY TABLE TO korin;
 
 
@@ -30,19 +28,29 @@ GRANT DELETE ANY TABLE TO korin;
 --Код відповідь:
 CREATE TABLE users (
 user_id NUMBER(8) NOT NULL,
-user_name VARCHAR(8) NOT NULL
+user_name VARCHAR(8) NOT NULL,
+user_birthdate date NOT NULL,
+user_password VARCHAR(10) NON NULL
 );
 ALTER TABLE users ADD CONSTRAINT users_id_pk PRIMARY KEY (users_id);
-
+ALTER TABLE users ADD CONSTRAINT date_of_birth_check CHECK (user_birthdate < '1997-01-01');
+ALTER TABLE users ADD CONSTRAINT password_check CHECK(user_password REGEXP_LIKE('^[0-9]{10}');
+                                                      
 CREATE TABLE news (
 news_id NUMBER(8) NOT NULL,
-news_heder VARCHAR(8) NOT NULL
+news_heder VARCHAR(8) NOT NULL CHECK (price > 0),
+news_body VARCHAR(255) NOT NULL,
+publish_date date NOT NULL
 );
 ALTER TABLE news ADD CONSTRAINT news_id_pk PRIMARY KEY (news_id);
 
+                                                      
 CREATE TABLE users_news(
 user_id_fk NUMBER(8) NOT NULL,
-news_id_fk NUMBER(8) NOT NULL
+news_id_fk NUMBER(8) NOT NULL,
+news_heder VARCHAR(8) NOT NULL CHECK (price > 0),
+news_body VARCHAR(255) NOT NULL,
+publish_date date NOT NULL
 );
 ALTER TABLE users_news ADD CONSTRAINT user_id_news_id_pk PRIMARY KEY (user_id_fk, news_id_fk);
 ALTER TABLE users_news ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id_fk) REFERENCE (users_id);
@@ -56,7 +64,7 @@ ALTER TABLE users_news ADD CONSTRAINT news_id_fk FOREIGN KEY (user_id_fk) REFERE
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 GRANT CREATE ANY TABLE TO korin;
-GRANT UPDATE ANY TABLE TO korin;
+GRANT INSERT ANY TABLE TO korin;
 GRANT SELECT ANY TABLE TO korin;
 
 
@@ -68,11 +76,11 @@ GRANT SELECT ANY TABLE TO korin;
 ---------------------------------------------------------------------------*/
 
 --Код відповідь:
-
-
-
-
-
+PROJECT prod_name
+TIMES Products
+WHERE prod_id NOT IN
+( PROJECT OrderItems.prod_id 
+ TIMES OrderItems )
 
 /*---------------------------------------------------------------------------
 3.b. 
@@ -83,11 +91,14 @@ GRANT SELECT ANY TABLE TO korin;
 ---------------------------------------------------------------------------*/
 
 --Код відповідь:
-SELECT
-    MAX(PROD_NAME)
+SELECT PROD_NAME
 FROM PRODUCTS
-WHERE PRODUCTS.PROD_ID = ORDERS.PROD_ID
-    
+WHERE 
+    LENGTH(RTRIM(PROD_NAME)) = (SELECT
+    MAX(LENGTH(RTRIM(PROD_NAME)))
+    FROM PRODUCTS, ORDERITEMS 
+    WHERE PRODUCTS.PROD_ID = ORDERITEMS.PROD_ID
+    );    
 
 /*---------------------------------------------------------------------------
 c. 
@@ -97,7 +108,7 @@ c.
 
 ---------------------------------------------------------------------------*/
 --Код відповідь:
-SELECT CUST_NAME AS 'client_name',
-        CUST_EMAIL AS 'client_name'
-FROM CUSTOMERS 
-WHERE ORDERS.CUST_ID = CUSTOMER.CUST_ID 
+SELECT TRIM(CUST_NAME) || ' ' || TRIM(CUST_EMAIL) AS client_name
+FROM CUSTOMERS
+WHERE CUST_ID IN (SELECT CUST_ID
+                        FROM ORDERS);
