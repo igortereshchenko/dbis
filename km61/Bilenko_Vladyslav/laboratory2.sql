49be011ca4f22ae0341d815f8451ceffaf39b46c
Vladyslav <bilenko.99@mail.ru>
2018-04-03 11:10:56 +0300

    Update laboratory2.sql

--- km61/Bilenko_Vladyslav/laboratory2.sql
+++ km61/Bilenko_Vladyslav/laboratory2.sql
@@ -1,2 +1,31 @@
 -- LABORATORY WORK 2
 -- BY Bilenko_Vladyslav
+/*---------------------------------------------------------------------------
+1. Вивести ключ покупця та ключ продукту за умови, що customer купив більше 5 одиниць цього продукту.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+select orders.cust_id, orderitems.prod_id
+from orders join orderitems
+on orderitems.order_item > 5;
+
+
+
+
+
+
+  
+/* --------------------------------------------------------------------------- 
+2.  Вивести ключ постачальника на номери замовлень, що містять більше одного з його товарів.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+
+select products.vend_id, orderitems.ORDER_NUM
+from products left join orderitems
+on orderitems.ORDER_NUM in (
+select count(prod_id) from orderitems
+where 
+(vend_id = prod_id) and count(prod_id) > 2 );
