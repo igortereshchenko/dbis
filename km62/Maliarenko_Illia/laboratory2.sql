f628438b3fcfded3eecc0281af8782545b1b8196
Illya <illya123456@outlook.com>
2018-04-05 13:09:42 +0300

    Update laboratory2.sql

--- km62/Maliarenko_Illia/laboratory2.sql
+++ km62/Maliarenko_Illia/laboratory2.sql
@@ -1,2 +1,31 @@
--- LABORATORY WORK 2
--- BY Maliarenko_Illia
+/*---------------------------------------------------------------------------
+1. Вивести ім'я покупця, номер замовлення та назву продукту, що найменше було куплено у кожному замовленні покупця.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+select customers.cust_name, orders.order_num, products.prod_name
+from customers, orders, orderitems, products
+where orderitems.order_num in 
+
+select 
+in (select order_num, min(orderitems.quantity) as minquant
+            from products inner join orderitems using(prod_id)
+              inner join orders using(order_num)
+            group by order_num)
+
+
+
+
+
+
+  
+/* --------------------------------------------------------------------------- 
+2.  Вивести ключ постачальника, що продавав свої продукти лише трьом покупцям.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+select products.VEND_ID
+from orders inner join orderitems using(order_num)
+  inner join products using(prod_id)
+group by products.VEND_ID
+having count(distinct orders.cust_id) = 3
