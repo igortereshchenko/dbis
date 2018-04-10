d21c15fda26aaeb98bd55950b8c8bb8e764df407
Vadym <vege1wgw@gmail.com>
2018-04-10 11:50:10 +0200

    Update laboratory1.sql

--- km51/Hevlich_Vadym/laboratory1.sql
+++ km51/Hevlich_Vadym/laboratory1.sql
@@ -1,7 +1,3 @@
--- LABORATORY WORK 1
--- BY Hevlich_Vadym
-
-
 /*---------------------------------------------------------------------------
 1.Створити схему даних з назвою – прізвище студента, у якій користувач може: 
 змінювати структуру таблиць та видаляти дані.
@@ -51,3 +47,124 @@ ALTER TABLE  Hotel
 
 ALTER TABLE  Tourist
   ADD CONSTRAINT hotel_fk FOREIGN KEY (hotel_id) REFERENCES Hotel (hotel_id);
+
+
+/* --------------------------------------------------------------------------- 
+3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
+внесення даних у таблиці та виконання вибірок використовуючи команду ALTER/GRANT. 
+Згенерувати базу даних використовуючи код з теки OracleScript та виконати запити: 
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+GRANT INSERT ANY TABLE TO hevlich;
+
+
+/*---------------------------------------------------------------------------
+3.a. 
+Як звуть покупця, що не купив найдорожчий продукт.
+Виконати завдання в Алгебрі Кодда. 
+4 бали
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+
+-- SQL:
+
+SELECT
+    CUSTOMERS.CUST_NAME,
+FROM CUSTOMERS,
+    ORDERS,
+    ORDERITEMS,
+    PRODUCTS,
+    (
+    SELECT
+        MAX(PRODUCTS.PROD_PRICE) AS PRICE, 
+        CUSTOMERS.CUST_ID AS CUST_ID
+    FROM CUSTOMERS,
+        ORDERS,
+        ORDERITEMS,
+        PRODUCTS
+    WHERE CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+    AND ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+    AND ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID) MAX_PROD_PRICE
+WHERE CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+    AND ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+    AND ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID
+    AND PRODUCTS.PROD_PRICE <  MAX_PROD_PRICE.PRICE;
+    
+
+-- Алгебра Кодда:
+    
+PROJECT (
+    CUSTOMERS TIMES ORDERS TIMES ORDERITEMS TIMES PRODUCTS TIMES PROJECT (
+        CUSTOMERS TIMES ORDERS TIMES ORDERITEMS TIMES PRODUCTS
+        (
+        WHERE CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+        AND ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+        AND ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID) MAX_PROD_PRICE){
+            MAX(PRODUCTS.PROD_PRICE) AS PRICE, 
+            CUSTOMERS.CUST_ID AS CUST_ID
+        }
+    (WHERE CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+        AND ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+        AND ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID
+        AND PRODUCTS.PROD_PRICE <  MAX_PROD_PRICE.PRICE)
+){
+    CUSTOMERS.CUST_NAME,
+};
+
+
+
+
+/*---------------------------------------------------------------------------
+3.b. 
+Вивести номер замовлення та назву товару у даному замовленні, що має найнижчу ціну у рамках замовлення.
+Виконати завдання в SQL. 
+4 бали
+
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+
+SELECT DISTINCT
+    ORDERS.ORDER_NUM,
+    MIN_PRICES.PROD_NAME
+FROM ORDERS, ODERITEMS, PRODUCTS, (
+    SELECT
+        MIN(ORDERITEMS.QUANTITY * ORDERITEMS.ITEM_PRICE) AS MIN_PRICE,
+        PRODUCTS.PROD_ID
+    FROM ORDERS, ORDERITEMS, PRODUCTS
+    WHERE ORDERS.ORDER_NUM = ODERITEMS.ORDER_NUM,
+        ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID
+    GROUP BY PRODUCTS.PROD_ID) MIN_PRICES
+WHERE ORDERS.ORDER_NUM = ODERITEMS.ORDER_NUM
+    AND ODERITEMS.PROD_ID = PRODUCTS.PROD_ID
+    AND MIN_PRICES.PROD_ID = PRODUCTS.PROD_ID;
+
+
+
+
+
+
+
+
+
+/*---------------------------------------------------------------------------
+c. 
+Вивести країну та пошту покупця, як єдине поле client_name у нижньому регістрі, для тих покупців, що купляли продукти у постачальника з іменем "James". 
+Виконати завдання в SQL. 
+4 бали
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+SELECT DISTINCT
+    CUSTOMERS.CUST_COUNTRY AS "client_name"
+FROM CUSTOMERS, ORDERS, ORDERITEMS, PRODUCTS
+WHERE CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+    AND ORDERS.ORDER_NUM = ODERITEMS.ORDER_NUM
+    AND ODERITEMS.PROD_ID = PRODUCTS.PROD_ID
+    AND PRODUCTS.VEND_ID IS NOT NULL;
