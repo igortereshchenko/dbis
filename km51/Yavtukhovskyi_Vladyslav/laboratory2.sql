cd300cefe3c98c38e12f9855c2ad8f03f2913a80
Yavtukhovskiy Vladislav <yavtukhovskiyv@gmail.com>
2018-04-10 11:09:56 +0300

    Add files via upload

--- km51/Yavtukhovskyi_Vladyslav/laboratory2.sql
+++ km51/Yavtukhovskyi_Vladyslav/laboratory2.sql
@@ -1,2 +1,36 @@
--- LABORATORY WORK 2
--- BY Yavtukhovskyi_Vladyslav
+﻿/*---------------------------------------------------------------------------
+1. Вивести ключ покупця, кількість його замовлень та загальну вартість куплених ним товарів.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+SELECT customers.cust_id, count(*), SUM(orderitems.item_price*orderitems.quantity)
+FROM customers join orders on customers.cust_id=orders.cust_id
+join orderitems on orders.order_num=orderitems.order_num
+GROUP BY customers.cust_id;
+
+
+
+
+
+
+
+
+
+
+/*---------------------------------------------------------------------------
+2.  Вивести ключ продукту, що міститься у найдешевших замовленнях покупців, що проживають в Америці.
+
+---------------------------------------------------------------------------*/
+
+
+
+--Код відповідь:
+SELECT products.prod_id 
+FROM (SELECT orders.order_num, SUM(orderitems.item_price*orderitems.quantity) AS minim
+FROM customers join orders on customers.cust_id=orders.cust_id
+join orderitems on orders.order_num=orderitems.order_num
+join products on orderitems.prod_id=products.prod_id
+WHERE customers.cust_country='USA'
+GROUP BY orders.order_num)
+HAVING minim = MIN(minim);
