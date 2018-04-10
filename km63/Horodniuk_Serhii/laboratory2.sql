31d9ac461280c116ddf28aec445c6b24015b4aec
SergFors <22may1999@ukr.net>
2018-04-10 14:59:50 +0300

    Update laboratory2.sql

--- km63/Horodniuk_Serhii/laboratory2.sql
+++ km63/Horodniuk_Serhii/laboratory2.sql
@@ -1,2 +1,33 @@
 -- LABORATORY WORK 2
 -- BY Horodniuk_Serhii
+
+/*---------------------------------------------------------------------------
+1. Вивести ключ постачальника, кількість його продуктів та загальну вартість проданих ним продуктів.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+SELECT vendors.vend_id, products.prod_id, products.prod_price
+FROM PRODUCTS JOIN VENDORS
+  ON PRODUCTS.VEND_ID = VENDORS.VEND_ID
+GROUP BY VENDORS.VEND_ID, PRODUCTS.PROD_ID, PRODUCTS.PROD_PRICE
+HAVING PRODUCTS.PROD_ID = COUNT(PRODUCTS.PROD_ID) AND
+PRODUCTS.PROD_PRICE = SUM(PRODUCTS.PROD_PRICE);
+
+
+
+/*---------------------------------------------------------------------------
+2.  Вивести ключ покупця, замовлення якого містить найдешевший продукт.
+
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+SELECT CUSTOMERS.CUST_ID
+FROM CUSTOMERS JOIN ORDERS
+  ON CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+  JOIN ORDERITEMS 
+  ON ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+  JOIN PRODUCTS
+  ON ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID
+WHERE PRODUSTS.PROD_PRICE = MIN(PRODUCTS.PROD_PRICE);
