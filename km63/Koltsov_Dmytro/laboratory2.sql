33038bc65b5befc4c2e8eb2dacd6fb6ce22a0853
Dmitriy Koltsov <dik_2010@ukr.net>
2018-04-10 15:20:14 +0300

    Update laboratory2.sql

--- km63/Koltsov_Dmytro/laboratory2.sql
+++ km63/Koltsov_Dmytro/laboratory2.sql
@@ -1,2 +1,52 @@
 -- LABORATORY WORK 2
 -- BY Koltsov_Dmytro
+/*---------------------------------------------------------------------------
+1. Вивести ключ постачальника, кількість його продуктів та загальну вартість проданих ним продуктів.
+
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+SELECT 
+    PRODUCTS.VEND_ID,
+    COUNT (PRODUCTS.VEND_ID) as ADDf
+    FROM PRODUCTS
+    GROUP BY  PRODUCTS.VEND_ID;
+    
+SELECT
+    SUM (ORDERITEMS.QUANTITY*ORDERITEMS.ITEM_PRICE) AS INITR,
+    PRODUCTS.VEND_ID
+    FROM
+    ORDERITEMS JOIN PRODUCTS
+        ON ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID
+    JOIN VENDORS
+        ON PRODUCTS.VEND_ID = VENDORS.VEND_ID
+    GROUP BY PRODUCTS.VEND_ID
+    
+
+
+
+
+
+
+
+
+
+/*---------------------------------------------------------------------------
+2.  Вивести ключ покупця, замовлення якого містить найдешевший продукт.
+
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+SELECT DISTINCT
+    CUSTOMERS.CUST_ID
+    FROM
+    CUSTOMERS JOIN ORDERS
+        ON CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+    JOIN ORDERITEMS
+        ON ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+     
+    WHERE ORDERITEMS.ITEM_PRICE IN (SELECT 
+                MIN(ITEM_PRICE)
+                FROM ORDERITEMS) 
+    
