15589c70c281cbaa10d8d5b23173e5267746ab2c
Григорий <loco_inverso@ukr.net>
2018-04-14 17:34:21 +0300

    Update laboratory1.sql

--- km62/Dmytrenko_Hryhorii/laboratory1.sql
+++ km62/Dmytrenko_Hryhorii/laboratory1.sql
@@ -27,41 +27,133 @@ GRANT INSERT ANY TABLE TO dmytrenko;
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 
-CREATE TABLE HUMAN(
-    human_age NUMBER(3) NOT NULL,
-    human_sex CHAR(1) NOT NULL,
-    human_country VARCHAR(20) NOT NULL,
-    human_name VARCHAR(20) NOT NULL
+/*==============================================================*/
+/* DBMS name:      ORACLE Version 11g                           */
+/* Created on:     14.04.2018 17:30:52                          */
+/*==============================================================*/
+
+alter table Human_has_phone
+   drop constraint FK_PHONE_OWNED_BY_HUMAN;
+
+alter table Human_has_phone
+   drop constraint FK_HUMAN_HAS_PHONE;
+
+alter table Sim_card
+   drop constraint FK_SIM_CARD1;
+
+alter table Sim_card
+   drop constraint FK_SIM_CARD2;
+
+drop table Human cascade constraints;
+
+drop index Human_has_phone_FK;
+
+drop index Human_has_phone2_FK;
+
+drop table Human_has_phone cascade constraints;
+
+drop table Phone_type cascade constraints;
+
+drop index second_sim_card_FK;
+
+drop index First_sim_card_FK;
+
+drop table Sim_card cascade constraints;
+
+/*==============================================================*/
+/* Table: Human                                                 */
+/*==============================================================*/
+create table Human 
+(
+   human_id             INTEGER              not null,
+   human_name           VARCHAR2(20)         not null,
+   human_sex            VARCHAR2(6)          not null,
+   human_age            INTEGER              not null,
+   human_country        VARCHAR2(15)         not null,
+   constraint PK_HUMAN primary key (human_id)
 );
 
-ALTER TABLE HUMAN
-    ADD CONSTRAINT human_pk PRIMARY KEY (human_name);
-    
-CREATE TABLE PHONE_TYPE(
-    phone_type_name VARCHAR(20) NOT NULL,
-    year_of_creation number(4) NOT NULL,
-    creator_name VARCHAR(20) NOT NULL,
-    country VARCHAR(20) NOT NULL
+/*==============================================================*/
+/* Table: Human_has_phone                                       */
+/*==============================================================*/
+create table Human_has_phone 
+(
+   phone_type_name      VARCHAR2(15)         not null,
+   human_id             INTEGER              not null,
+   constraint PK_HUMAN_HAS_PHONE primary key (phone_type_name, human_id)
 );
 
-ALTER TABLE PHONE_TYPE
-    ADD CONSTRAINT phone_type_name_pk PRIMARY KEY (phone_type_name);
-    
-CREATE TABLE HUMAN_PHONE(
-    human_name_fk VARCHAR(20) NOT NULL,
-    phone_type_name_fk VARCHAR(20) NOT NULL,
-    human_own_phone VARCHAR(20) NOT NULL
-    phone_type VARCHAR(20) NOT NULL
+/*==============================================================*/
+/* Index: Human_has_phone2_FK                                   */
+/*==============================================================*/
+create index Human_has_phone2_FK on Human_has_phone (
+   human_id ASC
 );
 
-ALTER TABLE HUMAN_PHONE
-    ADD CONSTRAINT human_own_phone_pk PRIMARY KEY (human_name_fk, phone_type_name_fk);
-    
-ALTER TABLE HUMAN_PHONE
-    ADD CONSTRAINT human_fk FOREIGN KEY (human_name_fk) REFERENCES HUMAN (human_name);
-    
-ALTER TABLE HUMAN_PHONE
-    ADD CONSTRAINT phone_fk FOREIGN KEY (phone_type_name_fk) REFERENCES PHONE_TYPE (phone_type_name);
+/*==============================================================*/
+/* Index: Human_has_phone_FK                                    */
+/*==============================================================*/
+create index Human_has_phone_FK on Human_has_phone (
+   phone_type_name ASC
+);
+
+/*==============================================================*/
+/* Table: Phone_type                                            */
+/*==============================================================*/
+create table Phone_type 
+(
+   phone_type_name      VARCHAR2(15)         not null,
+   year_of_creation     INTEGER              not null,
+   creator_name         VARCHAR2(20)         not null,
+   country              VARCHAR2(15),
+   constraint PK_PHONE_TYPE primary key (phone_type_name)
+);
+
+/*==============================================================*/
+/* Table: Sim_card                                              */
+/*==============================================================*/
+create table Sim_card 
+(
+   human_id1_fk         INTEGER              not null,
+   human_id2_fk         INTEGER              not null,
+   sim_number           INTEGER              not null,
+   constraint PK_SIM_CARD primary key (human_id1_fk, human_id2_fk, sim_number)
+);
+
+/*==============================================================*/
+/* Index: First_sim_card_FK                                     */
+/*==============================================================*/
+create index First_sim_card_FK on Sim_card (
+   human_id2_fk ASC
+);
+
+/*==============================================================*/
+/* Index: second_sim_card_FK                                    */
+/*==============================================================*/
+create index second_sim_card_FK on Sim_card (
+   human_id1_fk ASC
+);
+
+alter table Human_has_phone
+   add constraint FK_PHONE_OWNED_BY_HUMAN foreign key (phone_type_name)
+      references Phone_type (phone_type_name)
+      on delete cascade;
+
+alter table Human_has_phone
+   add constraint FK_HUMAN_HAS_PHONE foreign key (human_id)
+      references Human (human_id)
+      on delete cascade;
+
+alter table Sim_card
+   add constraint FK_SIM_CARD1 foreign key (human_id2_fk)
+      references Human (human_id)
+      on delete cascade;
+
+alter table Sim_card
+   add constraint FK_SIM_CARD2 foreign key (human_id1_fk)
+      references Human (human_id)
+      on delete cascade;
+
 /* --------------------------------------------------------------------------- 
 3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
 внесення даних у таблиці та виконання вибірок використовуючи команду ALTER/GRANT. 
@@ -82,7 +174,13 @@ GRANT SELECT ANY TABLE TO dmytrenko;
 ---------------------------------------------------------------------------*/
 
 --Код відповідь:
-SELECT max(prod_price) FROM Products WHERE prod_id in (SELECT prod_id FROM Products MINUS SELECT prod_id FROM Orderitems )
+
+PROJECT Products
+WHERE Products.prod_id IN
+(PROJECT Products { Products.prod_id }
+MINUS
+PROJECT Orderitems { Orderitems.prod_id })
+{ MAX(Products.prod_price)}
 
 /*---------------------------------------------------------------------------
 3.b. 
@@ -94,14 +192,11 @@ SELECT max(prod_price) FROM Products WHERE prod_id in (SELECT prod_id FROM Produ
 
 --Код відповідь:
 
-RENAME(
-PROJECT
-CUSTOMERS TIMES ORDERITEMS TIMES ORDERS
-WHERE ORDERS.CUST_ID = CUSTOMERS.CUST_ID
-AND ORDERITEMS.ORDER_NUM = ORDERS.ORDER_NUM
-AND ORDERITEMS.item_price in(PROJECT ORDERITEMS{MAX(ORDERITEMS.item_price)})
-{CUST_NAME}
-)cust_name->Customer_name
+SELECT CUST_NAME AS Customer_name
+FROM CUSTOMERS, ORDERITEMS, ORDERS
+    WHERE ORDERS.CUST_ID = CUSTOMERS.CUST_ID
+    AND ORDERITEMS.ORDER_NUM = ORDERS.ORDER_NUM
+    AND ORDERITEMS.ITEM_PRICE in (Select max(ITEM_PRICE) From ORDERITEMS);
 
 /*---------------------------------------------------------------------------
 c. 
@@ -112,6 +207,12 @@ c.
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 
-
-RENAME( PROJECT Vendors WHERE vend_id in (PROJECT Vendors{vend_id} MINUS PROJECT Products{vend_id} ) {vend_name || '_' || vend_country} )vend_name || '_' || vend_country->vendor_name
-
+SELECT (TRIM(Vendors.vend_name) || ' ' || TRIM(Vendors.vend_country)) as vendor_name
+FROM Vendors
+    WHERE Vendors.vend_id IN (
+        SELECT Vendors.vend_id
+        FROM Vendors    
+            MINUS        
+        SELECT Products.vend_id
+        FROM Products);
+        
