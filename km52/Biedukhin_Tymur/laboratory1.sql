d6e8b582e2958718f7aeab7ff737d421c63fbadb
Teimurazzz <timur4s@mail.ru>
2018-04-16 15:15:06 +0300

    Update laboratory1.sql

--- km52/Biedukhin_Tymur/laboratory1.sql
+++ km52/Biedukhin_Tymur/laboratory1.sql
@@ -7,7 +7,13 @@
 
 ---------------------------------------------------------------------------*/
 --Код відповідь:
-CREATE USER bedukhin IDENTIFIED BY bedukhinpass;
+CREATE USER bedukhin IDENTIFIED BY bedukhinpass
+DEFAULT TABLESPACE "USERS"
+TEMPORARY TABLESPACE "TEMP"
+QUOTA 100M ON USERS;
+
+GRANT "CONNECT" TO bedukhin;
+
 GRANT UPDATE ANY TABLE TO bedukhin;
 
 /*---------------------------------------------------------------------------
@@ -18,23 +24,40 @@ GRANT UPDATE ANY TABLE TO bedukhin;
 
 ---------------------------------------------------------------------------*/
 --Код відповідь:
+GRANT CREATE ANY TABLE TO bedukhin;
+GRANT ALTER ANY TABLE TO bedukhin;
 
 CREATE TABLE students 
 (
   student_id varchar2(50) NOT NULL,
-  student_name varchar2(50)
+  student_name varchar2(50),
+  student_phone varchar(50),
+  room_number_fk 
 );
 
 CREATE TABLE computers
 (
-  computer_serial_number varchar2(50) NOT NULL
+  computer_serial_number varchar2(50) NOT NULL,
+  computer_model varchar(50),
+  computer_is_working bool,
+  student_id_fk
 );
 
 CREATE TABLE rooms
 (
   room_number number(3) NOT NULL,
-  hostel_number NOT NULL
+  hostel_number_fk, 
+  room_floor number(2),
+  room_id number(1) NOT NULL
 );
+
+CREATE TABLE hostels 
+{
+hostel_number number(3) NOT NULL,
+hostel_street varchar(50),
+hostel_university_name varchar(50)
+hostel_id number(10) NOT NULL
+}
 
 ALTER TABLE students 
   ADD CONSTRAINT students_pk PRIMARY KEY (student_id);
@@ -43,13 +66,19 @@ ALTER TABLE computers
   ADD CONSTRAINT computers_pk PRIMARY KEY (computer_serial_number);
   
 ALTER TABLE rooms 
-  ADD CONSTRAINT rooms_pk PRIMARY KEY (room_number);  
+  ADD CONSTRAINT rooms_pk PRIMARY KEY (room_id);  
+  
+ALTER TABLE hostels 
+  ADD CONSTRAINT hostels_pk PRIMARY KEY (hostel_id);  
  
 ALTER TABLE students 
-   ADD CONSTRAINT students_computer_fk FOREIGN KEY (computer_serial_number) REFERENCES computers;
+   ADD CONSTRAINT students_rooms_fk FOREIGN KEY (room_number_fk) REFERENCES rooms(room_number);
 
-ALTER TABLE students 
-   ADD CONSTRAINT students_room_fk FOREIGN KEY (computer_pk) REFERENCES computers;
+ALTER TABLE rooms
+   ADD CONSTRAINT rooms_hostels_fk FOREIGN KEY (hostel_number_fk) REFERENCES hostels(hostel_number);
+   
+ALTER TABLE computers
+   ADD CONSTRAINT computers_students_fk FOREIGN KEY (student_id_fk) REFERENCES students(student_id);
    
    
 
@@ -88,10 +117,6 @@ GRANT SELECT ANY TABLE TO bedukhin;
 
 
 
-
-
-
-
 /*---------------------------------------------------------------------------
 3.b. 
 Який PROD_ID товару, з найдовшою назвою?
@@ -101,12 +126,8 @@ GRANT SELECT ANY TABLE TO bedukhin;
 ---------------------------------------------------------------------------*/
 
 --Код відповідь:
-
-
-
-
-
-
+  
+  
 
 
 
