225d7da145051d1bc7a39f85e79589cc558f0030
AnnaKuzina <koshkacd@gmail.com>
2018-04-19 00:10:37 +0300

    Update laboratory1.sql

--- km51/Kuzina_Anna/laboratory1.sql
+++ km51/Kuzina_Anna/laboratory1.sql
@@ -36,33 +36,236 @@ GRANT ALTER ANY TABLE TO kuzina;
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 
-CREATE TABLE BOOK(
-  book_name VARCHAR(30) NOT NULL
+/*==============================================================*/
+/* DBMS name:      ORACLE Version 11g                           */
+/* Created on:     18.04.2018 22:43:55                          */
+/*==============================================================*/
+
+
+alter table books_have_pages
+   drop constraint books_have_pages_fk;
+
+alter table books_have_pages
+   drop constraint page_in_book_fk;
+
+alter table pages_have_rows
+   drop constraint pages_have_rows_fk;
+
+alter table pages_have_rows
+   drop constraint pages_rows_fk;
+
+drop table BOOKS cascade constraints;
+
+drop table PAGES cascade constraints;
+
+drop table STRING_ROW cascade constraints;
+
+drop index page_in_book_FK;
+
+drop index books_have_pages_FK;
+
+drop table books_have_pages cascade constraints;
+
+drop index pages_rows_FK;
+
+drop index pages_have_rows_FK;
+
+drop table pages_have_rows cascade constraints;
+
+/*==============================================================*/
+/* Table: BOOKS                                                 */
+/*==============================================================*/
+create table BOOKS 
+(
+   book_id              NUMBER(10)           not null,
+   book_name            VARCHAR2(40)         not null,
+   book__author         VARCHAR2(40)         not null,
+   constraint PK_BOOKS primary key (book_id)
 );
-ALTER TABLE BOOK
-  ADD CONSTRAINT book_pk PRIMARY KEY (book_name);
 
-CREATE TABLE PAGE(
-  page_name VARCHAR(30) NOT NULL
-);
-ALTER TABLE  PAGE
-  ADD CONSTRAINT page_pk PRIMARY KEY (paget_name);  
-
-
-CREATE TABLE BOOK_PAGE(
-  book_name_fk VARCHAR(30) NOT NULL,
-  page_name_fk VARCHAR(30) NOT NULL,
-  page_row NUMBER(8) NOT NULL
+/*==============================================================*/
+/* Table: PAGES                                                 */
+/*==============================================================*/
+create table PAGES 
+(
+   page_id              NUMBER(10)           not null,
+   page_number          NUMBER(4)            not null,
+   constraint PK_PAGES primary key (page_id)
 );
 
-ALTER TABLE  BOOK_PAGE
-  ADD CONSTRAINT book_page_pk PRIMARY KEY (book_name_fk,page_name_fk);  
-  
-ALTER TABLE  BOOK_PAGE
-  ADD CONSTRAINT book_fk FOREIGN KEY (book_name_fk ) REFERENCES BOOK (book_name);
-  
-ALTER TABLE  BOOK_PAGE
-  ADD CONSTRAINT page_fk FOREIGN KEY (page_name_fk) REFERENCES PAGE (page_name);
+/*==============================================================*/
+/* Table: STRING_ROW                                            */
+/*==============================================================*/
+create table STRING_ROW 
+(
+   row_id               NUMBER(10)           not null,
+   row_number           NUMBER(20)           not null,
+   constraint PK_STRING_ROW primary key (row_id)
+);
+
+/*==============================================================*/
+/* Table: books_have_pages                                      */
+/*==============================================================*/
+create table books_have_pages 
+(
+   row_id               NUMBER(10)           not null,
+   page_id              NUMBER(10)           not null,
+   book_id              NUMBER(10)           not null,
+   constraint PK_BOOKS_HAVE_PAGES primary key (row_id, page_id, book_id)
+);
+
+/*==============================================================*/
+/* Index: books_have_pages_FK                                   */
+/*==============================================================*/
+create index books_have_pages_FK on books_have_pages (
+   book_id ASC
+);
+
+/*==============================================================*/
+/* Index: page_in_book_FK                                       */
+/*==============================================================*/
+create index page_in_book_FK on books_have_pages (
+   row_id ASC,
+   page_id ASC
+);
+
+/*==============================================================*/
+/* Table: pages_have_rows                                       */
+/*==============================================================*/
+create table pages_have_rows 
+(
+   row_id               NUMBER(10)           not null,
+   page_id              NUMBER(10)           not null,
+   constraint PK_PAGES_HAVE_ROWS primary key (row_id, page_id)
+);
+
+/*==============================================================*/
+/* Index: pages_have_rows_FK                                    */
+/*==============================================================*/
+create index pages_have_rows_FK on pages_have_rows (
+   page_id ASC
+);
+
+/*==============================================================*/
+/* Index: pages_rows_FK                                         */
+/*==============================================================*/
+create index pages_rows_FK on pages_have_rows (
+   row_id ASC
+);
+
+alter table books_have_pages
+   add constraint books_have_pages_fk foreign key (book_id)
+      references BOOKS (book_id)
+      on delete cascade;
+
+alter table books_have_pages
+   add constraint page_in_book_fk foreign key (row_id, page_id)
+      references pages_have_rows (row_id, page_id)
+      on delete cascade;
+
+alter table pages_have_rows
+   add constraint pages_have_rows_fk foreign key (page_id)
+      references PAGES (page_id)
+      on delete cascade;
+
+alter table pages_have_rows
+   add constraint pages_rows_fk foreign key (row_id)
+      references STRING_ROW (row_id)
+      on delete cascade;
+
+ALTER TABLE BOOKS
+ADD CONSTRAINT check_book_id
+CHECK (book_id > 0); 
+   
+ALTER TABLE BOOKS
+ADD CONSTRAINT check_book_name
+CHECK (REGEXP_LIKE(book_name,'(\s?\w+)+'));
+
+ALTER TABLE BOOKS
+ADD CONSTRAINT check_book_author
+CHECK (REGEXP_LIKE(book__author,'([A-Z][a-z]+[\-\s]?){2,}'));
+
+
+ALTER TABLE BOOKS_HAVE_PAGES
+ADD CONSTRAINT check_row_id
+CHECK (row_id > 0); 
+
+ALTER TABLE BOOKS_HAVE_PAGES
+ADD CONSTRAINT check_page_id
+CHECK (page_id > 0); 
+
+ALTER TABLE BOOKS_HAVE_PAGES
+ADD CONSTRAINT check1_book_id
+CHECK (book_id > 0); 
+
+ALTER TABLE PAGES
+ADD CONSTRAINT check1_page_id
+CHECK (page_id > 0); 
+
+ALTER TABLE PAGES
+ADD CONSTRAINT check1_page_number
+CHECK (page_number > 0); 
+
+
+ALTER TABLE PAGES_HAVE_ROWS
+ADD CONSTRAINT check1_row_id
+CHECK (row_id > 0);
+
+ALTER TABLE PAGES_HAVE_ROWS
+ADD CONSTRAINT check2_page_id
+CHECK (page_id > 0);
+
+ALTER TABLE STRING_ROW
+ADD CONSTRAINT check2_row_id
+CHECK (row_id > 0); 
+
+ALTER TABLE STRING_ROW
+ADD CONSTRAINT check2_row_number
+CHECK (row_number > 0); 
+
+
+/*======INSERTS=======*/
+--Create BOOKS
+INSERT INTO BOOKS (book_id, BOOK_NAME, BOOK__AUTHOR)
+VALUES ('1', 'Typhoon', 'Joseph Conrad');
+INSERT INTO BOOKS (book_id, BOOK_NAME, BOOK__AUTHOR)
+VALUES ('2', 'Harry Potter', 'Joanne Rowling');  
+INSERT INTO BOOKS (book_id, BOOK_NAME, BOOK__AUTHOR)
+VALUES ('3', 'IRELAND', 'Tim Vicary');
+
+--Create PAGES
+INSERT INTO PAGES (PAGE_ID, PAGE_NUMBER)
+VALUES ('1', '10');
+INSERT INTO PAGES (PAGE_ID, PAGE_NUMBER)
+VALUES ('2', '11');  
+INSERT INTO PAGES (PAGE_ID, PAGE_NUMBER)
+VALUES ('8', '11'); 
+
+--Create STRING_ROW
+INSERT INTO STRING_ROW (ROW_ID, ROW_NUMBER)
+VALUES ('10', '17');
+INSERT INTO STRING_ROW (ROW_ID, ROW_NUMBER)
+VALUES ('20', '1');  
+INSERT INTO STRING_ROW (ROW_ID, ROW_NUMBER)
+VALUES ('80', '18'); 
+
+
+--Create PAGES_HAVE_ROWS
+INSERT INTO PAGES_HAVE_ROWS (ROW_ID, PAGE_ID)
+VALUES ('10', '1');
+INSERT INTO PAGES_HAVE_ROWS (ROW_ID, PAGE_ID)
+VALUES ('20', '2');  
+INSERT INTO PAGES_HAVE_ROWS (ROW_ID, PAGE_ID)
+VALUES ('80', '8'); 
+
+--Create BOOKS_HAVE_PAGES
+INSERT INTO BOOKS_HAVE_PAGES (ROW_ID, PAGE_ID, BOOK_ID)
+VALUES ('10', '1', '1');
+INSERT INTO BOOKS_HAVE_PAGES (ROW_ID, PAGE_ID, BOOK_ID)
+VALUES ('20', '2', '2');  
+INSERT INTO BOOKS_HAVE_PAGES (ROW_ID, PAGE_ID, BOOK_ID)
+VALUES ('80', '8', '3');
+
 
 
 
