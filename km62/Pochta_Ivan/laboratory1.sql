a71b018582f26b7e98b5b4b34c8d092ec9b708cb
John Pochta <johnpochta1@gmail.com>
2018-04-18 23:18:14 +0300

    Update laboratory1.sql

--- km62/Pochta_Ivan/laboratory1.sql
+++ km62/Pochta_Ivan/laboratory1.sql
@@ -34,42 +34,93 @@ GRANT INSERT ANY TABLE TO pochta;
 
 ---------------------------------------------------------------------------*/
 --Код відповідь:
-CREATE TABLE STUDENT(
-  student_id varchar(5) not null,
-  student_name varchar(25) not null
+CREATE TABLE STUDENTS(
+  student_id varchar(7) not null
 );
-ALTER TABLE STUDENT ADD CONSTRAINT student_pk PRIMARY KEY( student_id );
+INSERT INTO STUDENTS(STUDENT_ID) VALUES ('KM6220');
+INSERT INTO STUDENTS(STUDENT_ID) VALUES ('KM6218');
+INSERT INTO STUDENTS(STUDENT_ID) VALUES ('KM6219');
+INSERT INTO STUDENTS(STUDENT_ID) VALUES ('KM6210');
+CREATE TABLE TICKETS(
+  ticket_id varchar(15) not null
+);
+INSERT INTO TICKETS VALUES('880055535300000');
+INSERT INTO TICKETS VALUES('880055535300001');
+INSERT INTO TICKETS VALUES('880055535300002');
+INSERT INTO TICKETS VALUES('880055535300003');
 
-CREATE TABLE TICKET(
-  ticket_id varchar(5) not null,
-  train_id varchar(5) not null,
-  customer_id varchar(5) not null
+CREATE TABLE TRAINS(
+  train_id varchar(5) not null
 );
-ALTER TABLE TICKET ADD CONSTRAINT ticket_pk PRIMARY KEY (ticket_id);
+
+INSERT INTO TRAINS VALUES('00000');
+INSERT INTO TRAINS VALUES('00001');
+INSERT INTO TRAINS VALUES('00002');
+INSERT INTO TRAINS VALUES('00003');
+
+CREATE TABLE STATIONS(
+  station_id varchar(5) not null,
+  station_name varchar(50) not null
+);
+INSERT INTO STATIONS(station_id, station_name) VALUES('00000', 'Ladyzhyn');
+INSERT INTO STATIONS(station_id, station_name) VALUES('00001', 'Smila');
+INSERT INTO STATIONS(station_id, station_name) VALUES('00002', 'Turiysk');
+INSERT INTO STATIONS(station_id, station_name) VALUES('00003', 'Vinogradiv');
+
+CREATE TABLE TRAIN_HAS_TICKET(
+    ticket_fk varchar(15) not null,
+    train_fk varchar(5) not null
+);
+INSERT INTO TRAIN_HAS_TICKET(ticket_fk, train_fk) VALUES('880055535300000', '00000');
+INSERT INTO TRAIN_HAS_TICKET(ticket_fk, train_fk) VALUES('880055535300001', '00000');
+INSERT INTO TRAIN_HAS_TICKET(ticket_fk, train_fk) VALUES('880055535300002', '00000');
+INSERT INTO TRAIN_HAS_TICKET(ticket_fk, train_fk) VALUES('880055535300003', '00001');
+CREATE TABLE TRAIN_HAS_STATION(
+  train_fk varchar(5) not null,
+  station_fk varchar(5) not null
+);
+INSERT INTO TRAIN_HAS_STATION(train_fk, station_fk) VALUES('00000', '00001');
+INSERT INTO TRAIN_HAS_STATION(train_fk, station_fk) VALUES('00000', '00002');
+INSERT INTO TRAIN_HAS_STATION(train_fk, station_fk) VALUES('00000', '00003');
+INSERT INTO TRAIN_HAS_STATION(train_fk, station_fk) VALUES('00000', '00000');
 CREATE TABLE STUDENT_BUY_TICKET(
-  student_fk varchar(5) not null,
-  ticket_fk varchar(5) not null 
+  student_fk varchar(6) not null,
+  ticket_fk varchar(15) not null 
 );
-ALTER TABLE STUDENT_BUY_TICKET ADD CONSTRAINT sbt_pk PRIMARY KEY (ticket_fk);
-ALTER TABLE STUDENT_BUY_TICKET  ADD CONSTRAINT sbt_student_fk FOREIGN KEY (student_fk) REFERENCES  STUDENT(student_id);
-ALTER TABLE STUDENT_BUY_TICKET  ADD CONSTRAINT sbt_ticket_fk FOREIGN KEY (ticket_fk) REFERENCES  TICKET(ticket_id);
+INSERT INTO STUDENT_BUY_TICKET(ticket_fk, student_fk) VALUES('880055535300000', 'KM6220');
+INSERT INTO STUDENT_BUY_TICKET(ticket_fk, student_fk) VALUES('880055535300001', 'KM6219');
+INSERT INTO STUDENT_BUY_TICKET(ticket_fk, student_fk) VALUES('880055535300002', 'KM6218');
+INSERT INTO STUDENT_BUY_TICKET(ticket_fk, student_fk) VALUES('880055535300003', 'KM6210');
+ALTER TABLE STUDENTS add constraint students_pk primary key (student_id);
+ALTER TABLE TICKETS add constraint tickets_pk primary key (ticket_id);
+ALTER TABLE TRAINS add constraint trains_pk primary key (train_id);
+ALTER TABLE STATIONS add constraint stations_pk primary key (station_id);
+ALTER TABLE TRAIN_HAS_STATION add constraint train_has_station_pk primary key (train_fk, station_fk);
+ALTER TABLE TRAIN_HAS_STATION add constraint train_has_station_fk_train FOREIGN KEY (train_fk) references TRAINS(train_id);
+ALTER TABLE TRAIN_HAS_STATION add constraint train_has_station_fk_station FOREIGN KEY (station_fk) references STATIONS(station_id); 
+ALTER TABLE STUDENT_BUY_TICKET add constraint student_buy_ticket_pk primary key ( ticket_fk );
+ALTER TABLE STUDENT_BUY_TICKET add constraint student_buy_ticket_fk_student FOREIGN KEY ( student_fk ) references STUDENTS(student_id);
+ALTER TABLE STUDENT_BUY_TICKET add constraint student_buy_ticket_fk_ticket FOREIGN KEY ( ticket_fk ) references TICKETS(ticket_id);
+ALTER TABLE TRAIN_HAS_TICKET add constraint train_has_ticket_pk PRIMARY KEY ( ticket_fk ) ;
+ALTER TABLE TRAIN_HAS_TICKET add constraint train_has_ticket_fk_train FOREIGN KEY (train_fk) references TRAINS(train_id);
+ALTER TABLE TRAIN_HAS_TICKET add constraint train_has_ticket_fk_ticket FOREIGN KEY (ticket_fk) references TICKETS(ticket_id);
+alter table tickets
+  add constraint check_ticket_id 
+  check (REGEXP_LIKE(ticket_id, '^[0-9]{15}'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+alter table trains
+  add constraint check_train_id 
+  check (REGEXP_LIKE(train_id, '^[0-9]{5}'));
+alter table students
+  add constraint check_student_id 
+  check (REGEXP_LIKE(student_id, '^[A-Z]{2}[0-9]{4}'));
+alter table students drop constraint check_student_id;
+alter table stations
+  add constraint check_station_id 
+  check (REGEXP_LIKE(station_id ,'^[0-9]{5}'));
+alter table stations
+  add constraint check_station_name
+  check (REGEXP_LIKE(station_name ,'^[A-Z][a-z]{1,49}'));
 /* --------------------------------------------------------------------------- 
 3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
 внесення даних у таблиці та виконання вибірок використовуючи команду ALTER/GRANT. 
