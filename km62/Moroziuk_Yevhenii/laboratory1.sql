423b1e228ebfb168f9a7654a9735c1538565d6b2
yMoroziuk <fae09@mail.ru>
2018-04-19 00:28:48 +0300

    Update laboratory1.sql
    2nd fixed

--- km62/Moroziuk_Yevhenii/laboratory1.sql
+++ km62/Moroziuk_Yevhenii/laboratory1.sql
@@ -29,32 +29,184 @@ GRANT INSERT ANY TABLE TO student;
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 
-CREATE TABLE PERSON(
-person_name char(20) NOT NULL
-);
-CREATE TABLE TELEPHONE(
-tel_mark char(20) NOT NULL
-);
-CREATE TABLE PERSONS_TELEPHONE(
-person_name_fk char(20) NOT NULL,
-tel_mark_fk char(20) NOT NULL
-);
-ALTER TABLE PERSON
-  ADD CONSTRAINT person_name_pk PRIMARY KEY(person_name);
-  
-ALTER TABLE TELEPHONE
-  ADD CONSTRAINT tel_mark_pk PRIMARY KEY(tel_mark);
 
-ALTER TABLE PERSONS_TELEPHONE
-  ADD CONSTRAINT per_tel_pk PRIMARY KEY(person_name_fk, tel_mark_fk);
+CREATE TABLE mobile ( 
+        mobile_brand            VARCHAR2(12) NOT NULL,  
+        mobile_camera           NUMBER(2) NOT NULL, 
+        mobile_processor        VARCHAR(200) NOT NULL, 
+        CONSTRAINT mobile_phone_pk PRIMARY KEY ( mobile_brand) 
+    ); 
+   
+    ALTER TABLE mobile 
+        ADD CONSTRAINT mobile_price_ch CHECK ( length(mobile_brand) >= 3 ); 
+         
+     
+    CREATE TABLE Human (
+        human_id   NUMBER(8) NOT NULL, 
+        human_name VARCHAR(24) NOT NULL, 
+        birth      DATE,  
+        CONSTRAINT human_id_pk PRIMARY KEY ( human_id ) 
+    ); 
 
-ALTER TABLE PERSONS_TELEPHONE
-  ADD CONSTRAINT person_fk FOREIGN KEY(person_name_fk) REFERENCE PERSON(person_name);
+    ALTER TABLE human 
+        ADD CONSTRAINT human_name_ch CHECK ( length(human_name) >= 2 ); 
 
-ALTER TABLE PERSONS_TELEPHONE
-  ADD CONSTRAINT tel_mark_fk FOREIGN KEY(tel_mark_fk) REFERENCE PERSON(tel_mark);
-
-  
+     
+    CREATE TABLE Humans_phone ( 
+        human_id_fk          NUMBER(8) NOT NULL, 
+        mobile_brand_fk      VARCHAR2(12) NOT NULL  
+    ); 
+     
+    ALTER TABLE Humans_phone 
+        ADD CONSTRAINT human_with_mobile_pk PRIMARY KEY ( human_id_fk, 
+        mobile_brand_fk ); 
+     
+    ALTER TABLE Humans_phone 
+        ADD CONSTRAINT mobile_fk FOREIGN KEY ( mobile_brand_fk ) 
+            REFERENCES mobile ( mobile_brand ); 
+     
+    ALTER TABLE Humans_phone 
+        ADD CONSTRAINT human_fk FOREIGN KEY ( human_id_number_fk ) 
+            REFERENCES human ( human_id_number ); 
+          
+     
+    CREATE TABLE shop ( 
+        mobile_phone_fk VARCHAR2(14) NOT NULL,
+        mobile_price    NUMBER(5) NOT NULL,  
+        packages_p      NUMBER(4) NOT NULL,  
+        accessories_p   NUMBER(4) NOT NULL 
+    ); 
+     
+    ALTER TABLE shop 
+        ADD CONSTRAINT mobile_from_shop_pk PRIMARY KEY ( mobile_phone_fk, 
+        mobile_price ); 
+     
+    ALTER TABLE shop 
+        ADD CONSTRAINT mob_phone_fk FOREIGN KEY ( mobile_phone_fk ) 
+            REFERENCES mobile ( mobile_brand ); 
+        
+ 
+    
+    INSERT INTO mobile ( 
+        mobile_brand,              
+        mobile_camera,            
+        mobile_processor  
+    ) VALUES ( 
+        'iPhone 8', 
+        12, 
+        'A11 bionic'
+        ); 
+     
+      INSERT INTO mobile ( 
+        mobile_brand,              
+        mobile_camera,            
+        mobile_processor  
+    ) VALUES ( 
+        'SG S7', 
+        12, 
+        'Exynos 8'
+        ); 
+        
+    INSERT INTO mobile ( 
+        mobile_brand,              
+        mobile_camera,            
+        mobile_processor  
+    ) VALUES ( 
+        'Mi Mix 2s', 
+        12, 
+        'SD 845'
+        ); 
+    
+     INSERT INTO human ( 
+        human_id, 
+        human_name, 
+        birth  
+    ) VALUES ( 
+        777777, 
+        'Yevhenii', 
+        TO_DATE('1999-03-10','YYYY-MM-DD') 
+    ); 
+    
+    INSERT INTO human ( 
+        human_id, 
+        human_name, 
+        birth  
+    ) VALUES ( 
+        888888, 
+        'Yevhenii8', 
+        TO_DATE('1999-03-10','YYYY-MM-DD') 
+    );
+    
+    INSERT INTO human ( 
+        human_id, 
+        human_name, 
+        birth  
+    ) VALUES ( 
+        999999, 
+        'Yevhenii9', 
+        TO_DATE('1999-03-10','YYYY-MM-DD') 
+    );
+    
+    
+    INSERT INTO Humans_phone ( 
+        human_id_fk, 
+        mobile_brand_fk
+    ) VALUES ( 
+        777777, 
+        'iPhone 8' 
+    ); 
+     
+    INSERT INTO Humans_phone ( 
+        human_id_fk, 
+        mobile_brand_fk
+    ) VALUES ( 
+        888888, 
+        'SG S7' 
+    );
+    
+    INSERT INTO Humans_phone ( 
+        human_id_fk, 
+        mobile_brand_fk
+    ) VALUES ( 
+        999999, 
+        'Mi Mix 2s' 
+    );  
+     
+    INSERT INTO mobile_shop ( 
+        mobile_phone_fk,
+        mobile_price,  
+        packages_p,  
+        accessories_p
+    ) VALUES ( 
+        'iPhone 8', 
+        '25000', 
+        999, 
+        500 
+    ); 
+     
+    INSERT INTO mobile_shop ( 
+        mobile_phone_fk,
+        mobile_price,  
+        packages_p,  
+        accessories_p
+    ) VALUES ( 
+        'SG S7', 
+        '15000', 
+        799, 
+        600 
+    );
+    
+    INSERT INTO mobile_shop ( 
+        mobile_phone_fk,
+        mobile_price,  
+        packages_p,  
+        accessories_p
+    ) VALUES ( 
+        'Mi Mix 2s', 
+        '22000', 
+        900, 
+        600 
+    );
 /* --------------------------------------------------------------------------- 
 3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
 внесення даних у таблиці та виконання вибірок використовуючи команду ALTER/GRANT. 
