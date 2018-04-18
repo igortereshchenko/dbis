0925ae0ad85639836535884def4f0ad6729bc43d
Samovilov <sergik241997@gmail.com>
2018-04-18 23:58:19 +0300

    Update laboratory1.sql

--- km63/Samovilov_Serhii/laboratory1.sql
+++ km63/Samovilov_Serhii/laboratory1.sql
@@ -23,21 +23,63 @@ alter user "Samovilov" quota 100mb on users ;
 4 бали
 ---------------------------------------------------------------------------*/
 --Код відповідь:
---create table 'fb_users'(
---log vchar2(15) not null,
- --passw char(10) not null
- --);
+
+
+create table fb_users
+    (
+        user_id number(6) not null,
+        user_name varchar2(20) not null
+        );
+
+CREATE TABLE facts
+    (
+        fact_name varchar2(20) not null,
+        fact_date date not null,     
+    );
+
+alter table facts
+add constraint fn_pk primary key (fact_name);
+
+
+create table fb_news
+    (
+            fact_name_fk varchar2(20) not null 
+            news_id number(6) not null,
+            news_topic varchar2(30) not null
+        );
+        
+    alter table fb_users
+        add constraint user_pk primary key (user_id) ;
+        
+    alter table fb_news
+        add constraint news_pk primary key (new_id);
+        
+    alter table fb_news
+        add constraint fn_fk foreign key ( fact_name_fk) references  facts (fact_name);   
+        
+CREATE TABLE fb_news_users
+    (
+        new_id_fk number(6) not null,
+        user_id_fk number(6) not null,     
+    );
+    alter table fb_news_users
+  add constraint fb_n_u_pk primary key (new_id_fk, user_id_fk);
+alter table fb_news_users
+  add constraint new_id_fk foreign key (new_id_fk) references  fb_news (new_id);
+alter table fb_news_users
+  add constraint user_id_fk foreign key (user_id_fk) references  fb_users (user_id);
 
 
 
-
-
-
-
-
-
-
-
+insert into fb_users (user_id, user_name) values ('356740','Va$Y@');
+insert into fb_users (user_id, user_name) values ('356921','Petr');
+insert into fb_users (user_id, user_name) values ('576040',' )i( O R A ');
+insert into facts (fact_name, fact_date) values ('Vibori2018','2018-03-01');
+insert into facts (fact_name, fact_date) values ('Polet_v_kosmos','1961-04-12');
+insert into facts (fact_name, fact_date) values ('Otchislenie','2017-01-15');
+insert into fb_news ( fact_name_fk, news_id, news_topic) values ('Vibori2018','000001', 'topic1');
+insert into fb_news ( fact_name_fk, news_id, news_topic) values ('Otchislenie','000002','topic2' );
+insert into fb_news ( fact_name_fk, news_id, news_topic) values ('Polet_v_kosmos','000003','topic3' );
 
 
 
