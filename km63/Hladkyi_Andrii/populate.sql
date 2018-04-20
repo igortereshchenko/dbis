62246e0188478481391fe630757424c08f1951bf
Andriy-Hladkiy <andriyha98@gmail.com>
2018-04-20 11:54:50 +0300

    Add files via upload

---
+++ km63/Hladkyi_Andrii/populate.sql
@@ -0,0 +1,46 @@
+insert into "Group" ("group_code") values ('EW7WGP');
+
+insert into "Group" ("group_code") values ('AYDJ1N');
+
+insert into "Group" ("group_code") values ('YWVN8K');
+
+
+
+insert into "Discipline" ("Discipline_name") values ('PM W81 1T3B3U3J6TSKE');
+
+insert into "Discipline" ("Discipline_name") values ('009T27BLQRTQXYGUEMD4');
+
+insert into "Discipline" ("Discipline_name") values ('60H9QWW939IJHJBUW6GF');
+
+
+insert into "Lection" ("lection_number", "student_id", "student_name", "student_group", "start_reading", "the_end_of_the_reading") values (16, 5, 'R 9919DIHD62Q221T3IG', 'FERF8T', '1408-9-20 7:43:59', '1423-2-24 9:14:5');
+
+insert into "Lection" ("lection_number", "student_id", "student_name", "student_group", "start_reading", "the_end_of_the_reading") values (13, 9, 'TLF29DN9BGNT0JUSA11K', 'FERF8T', '1761-12-7 9:18:27', '576-1-8 4:1:39');
+
+insert into "Lection" ("lection_number", "student_id", "student_name", "student_group", "start_reading", "the_end_of_the_reading") values (5, 9, 'TLF29DN9BGNT0JUSA11K', 'FERF8T', '787-7-31 4:18:20', '786-6-30 5:12:36');
+
+
+
+insert into "Students" ("student_id", "student_name", "student_year", "student_birthday", "student_group", "group_code", "lection_number", "student_email") values (9, 'TLF29DN9BGNT0JUSA11K', '157-11-23 1:15:30', '1527-9-28 8:51:11', 'FERF8T', 'YWVN8K', 0, 'SN76JXRSF LWNPFL0MQA2FX1BBIPE1');
+
+insert into "Students" ("student_id", "student_name", "student_year", "student_birthday", "student_group", "group_code", "lection_number", "student_email") values (19, 'W3TYQT5HQ6XTTP9HO1A6', '479-5-29 3:7:46', '1278-12-31 7:37:55', 'JHF05J', '87K83G', 19, 'JKRJX5MTUJOKY73U1KSFDC5O7GL3JK');
+
+insert into "Students" ("student_id", "student_name", "student_year", "student_birthday", "student_group", "group_code", "lection_number", "student_email") values (13, 'OUV3EAWXKX70A2MHBT9K', '1747-7-4 10:4:30', '485-5-12 1:31:34', 'XIJ3WV', 'PD9JFR', 4, 'J50BVQHT348L06WKQFKTGE338VEQVU');
+
+
+
+insert into "Assessments" ("student_id", "student_name", "student_group", "Discipline_name") values (9, 'TLF29DN9BGNT0JUSA11K', 'FERF8T', 'O8LA9BVO7YYR G MWPL6');
+
+insert into "Assessments" ("student_id", "student_name", "student_group", "Discipline_name") values (15, 'RR7XSMINPCV ITMTJ98E', 'JHF05J', '009T27BLQRTQXYGUEMD4');
+
+insert into "Assessments" ("student_id", "student_name", "student_group", "Discipline_name") values (3, 'W4Q1MOU3RMFTEPEQY4SO', 'NJ8VPT', 'LI245WMFATA F0REU8QM');
+
+
+
+insert into "Homework" ("homework_number", "student_id", "student_name", "student_group", "Discipline_name") values (19, 9, 'TLF29DN9BGNT0JUSA11K', 'FERF8T', 'OYADH7URY080JLXD0IHS');
+
+insert into "Homework" ("homework_number", "student_id", "student_name", "student_group", "Discipline_name") values (9, 0, 'DF7IHAK277W06YNFP5MT', 'QOV466', '009T27BLQRTQXYGUEMD4');
+
+insert into "Homework" ("homework_number", "student_id", "student_name", "student_group", "Discipline_name") values (3, 11, 'W4Q1MOU3RMFTEPEQY4SO', 'NJ8VPT', 'OYADH7URY080JLXD0IHS');
+
+
