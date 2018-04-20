03dabfaeb51aca119c9c248ef83adad3d6837585
Anton Kozyrev <sonara4mus@gmail.com>
2018-04-20 21:32:08 +0300

    Add checks

--- km62/Kozyriev_Anton/laboratory1.sql
+++ km62/Kozyriev_Anton/laboratory1.sql
@@ -38,55 +38,74 @@ GRANT INSERT ANY TABLE TO kozyrev;
 ---------------------------------------------------------------------------*/
 --Код відповідь:
 
-
 CREATE TABLE Customer
 (
-    cust_reserved_phone_number VARCHAR(12) NOT NULL,
-    cust_name VARCHAR(20) NOT NULL,
-    cust_registration_date VARCHAR(20) NOT NULL,
-    cust_email VARCHAR(30) NULL,
+    cust_reserved_phone_number VARCHAR(12) NOT NULL, -- ex. +xx(xxx)xxx-xx-xx
+    cust_name VARCHAR(20) NOT NULL, -- ex. Ivan Petrov
+    cust_registration_date VARCHAR(20) NOT NULL, -- ex. xx.xx.xxxx
+    cust_email VARCHAR(30) NULL -- ex. example_number99@sample.com
 );
 
-ALTER TABLE Customer ADD CONSTRAINT CustomerPK PRIMARY KEY(cust_reserved_phone_number, cust_name);
+ALTER TABLE Customer ADD CONSTRAINT CustomerPK PRIMARY KEY (cust_reserved_phone_number);
+
+ALTER TABLE Customer ADD CONSTRAINT Check_Customers_Number CHECK (REGEXP_LIKE(cust_reserved_phone_number, '^\+38\(0\d{2}\)\d{3}\-\d{2}\-\d{2}$'));
+ALTER TABLE Customer ADD CONSTRAINT Check_Customers_Name CHECK (REGEXP_LIKE(cust_name, '^\w+\s\w+$'));
+ALTER TABLE Customer ADD CONSTRAINT Check_Customers_Registration_Date CHECK (REGEXP_LIKE(cust_registration_date, '^([0-9]{2}\.){2}([0-9]{4})$'));
+ALTER TABLE Customer ADD CONSTRAINT Check_Customers_Email CHECK (REGEXP_LIKE(cust_email, '^((\w|\d)|\_)+\@(\w|\d)+\.(\w|\d)+$'));
+
 
 CREATE TABLE Phone
 (
-    phone_model VARCHAR(30) NOT NULL,
     phone_serial VARCHAR(20) NOT NULL, 
-    phone_brand VARCHAR(30) NOT NULL,
-    cust_reserved_phone_number VARCHAR(12) NOT NULL,
-    brand_name VARCHAR(30) NOT NULL,
+    brand_serial VARCHAR(30) NOT NULL,
+    reserved_phone_number VARCHAR(12) NOT NULL,
+    phone_model VARCHAR(30) NOT NULL,
     phone_price NUMBER NOT NULL,
     vend_id VARCHAR(30) NOT NULL
 );
 
-ALTER TABLE Phone ADD CONSTRAINT PhonePK PRIMARY KEY(phone_model, phone_serial);
-ALTER TABLE Phone ADD CONSTRAINT Phone_NumberFK FOREIGN KEY(cust_reserved_phone_number) REFERENCES Customer(cust_reserved_phone_number);
-ALTER TABLE Phone ADD CONSTRAINT Phone_BrandFK FOREIGN KEY(brand_name) REFERENCES PhoneBrand(brand_name);
+ALTER TABLE Phone ADD CONSTRAINT PhonePK PRIMARY KEY(phone_serial);
+
+ALTER TABLE Phone ADD CONSTRAINT Phone_NumberFK FOREIGN KEY(reserved_phone_number) REFERENCES Customer(cust_reserved_phone_number);
+ALTER TABLE Phone ADD CONSTRAINT Phone_BrandFK FOREIGN KEY (brand_serial) REFERENCES PhoneBrand(brand_serial);
 ALTER TABLE Phone ADD CONSTRAINT Phone_VendorFK FOREIGN KEY(vend_id) REFERENCES Vendor(vend_id);
+
+ALTER TABLE Phone ADD CONSTRAINT Check_Phone_Serial CHECK (REGEXP_LIKE(phone_serial, '^(\w|\d)+$'));
+ALTER TABLE Phone ADD CONSTRAINT Check_Phone_Brand CHECK (REGEXP_LIKE(brand_serial, '^(\w|\d)+$'));
+ALTER TABLE Phone ADD CONSTRAINT Check_Phone_Number CHECK (REGEXP_LIKE(reserved_phone_number, '^\+38\(0\d{2}\)\d{3}\-\d{2}\-\d{2}$'));
+ALTER TABLE Phone ADD CONSTRAINT Check_Phone_Model CHECK (REGEXP_LIKE(phone_model, '^(\w|\s|\d)+$'));
+ALTER TABLE Phone ADD CONSTRAINT Check_Phone_Price CHECK (phone_price >= 0);
+ALTER TABLE Phone ADD CONSTRAINT Check_Phone_Vend_ID CHECK (REGEXP_LIKE(vend_id, '^(\w|\d)+$'));
 
 CREATE TABLE PhoneBrand
 (
-    brand_company VARCHAR(30) NOT NULL,
+    brand_serial VARCHAR(30) NOT NULL,
     brand_name VARCHAR(30) NOT NULL,
-    brand_rating NUMBER NULL,
-    brand_state BOOLEAN NOT NULL,
+    brand_company VARCHAR(30) NOT NULL,
+    brand_rating NUMBER NULL
 );
 
-ALTER TABLE PhoneBrand ADD CONSTRAINT PhoneBrandPK PRIMARY KEY(brand_company, brand_name);
+ALTER TABLE PhoneBrand ADD CONSTRAINT PhoneBrandPK PRIMARY KEY(brand_serial);
+
+ALTER TABLE PhoneBrand ADD CONSTRAINT Check_Brand_Serial CHECK (REGEXP_LIKE(brand_serial, '^(\w|\d)+$'));
+ALTER TABLE PhoneBrand ADD CONSTRAINT Check_Brand_Name CHECK (REGEXP_LIKE(brand_name, '^(\w|\s)+$'));
+ALTER TABLE PhoneBrand ADD CONSTRAINT Check_Brand_Company CHECK (REGEXP_LIKE(brand_company, '^(\w|\s|\d)+$'));
+ALTER TABLE PhoneBrand ADD CONSTRAINT Check_Brand_Rating CHECK (brand_rating >= 0 AND brand_rating <= 10);
 
 CREATE TABLE Vendor
 (
     vend_id VARCHAR(30) NOT NULL,
     vend_name VARCHAR(30) NOT NULL,
-    brand_rating NUMBER NULL,
-    brand_state BOOLEAN NOT NULL,
+    vend_rating NUMBER NULL,
+    vend_address VARCHAR(30) NULL
 );
 
 ALTER TABLE Vendor ADD CONSTRAINT VendorPK PRIMARY KEY(vend_id);
 
-
-
+ALTER TABLE Vendor ADD CONSTRAINT Check_Vend_ID CHECK (REGEXP_LIKE(vend_id, '^(\w|\d)+$'));
+ALTER TABLE Vendor ADD CONSTRAINT Check_Vend_Name CHECK (REGEXP_LIKE(vend_name, '^(\w|\s|\d)+$'));
+ALTER TABLE Vendor ADD CONSTRAINT Check_Vend_Rating CHECK (vend_rating >= 0 AND vend_rating <= 10);
+ALTER TABLE Vendor ADD CONSTRAINT Check_Vend_Address CHECK (REGEXP_LIKE(vend_address, '^(\w|\s|\d)+$'));
   
 /* --------------------------------------------------------------------------- 
 3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
