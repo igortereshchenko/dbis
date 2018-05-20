9df7bca23e34fc2f48e0ebfba370dfc9e1bd91c6
Zlata Khodos <zlata_khodos@ukr.net>
2018-05-20 15:28:24 +0300

    Update studentpma~queries.sql

--- km62/Khodos_Zlata/studentpma~queries.sql
+++ km62/Khodos_Zlata/studentpma~queries.sql
@@ -54,15 +54,30 @@ person.person_id, note_name
 group by info.person_id, info.markers_date;
 
     
-CREATE VIEW user_notes 
-AS 
-    SELECT DISTINCT person.person_id, note_marker.note_name, marker.marker_start_point, marker_finish_point
-    FROM person
-    JOIN marker ON person.person_id = marker.person_id
-    JOIN note_marker ON marker.marker_id = note_marker.marker_id
-                        AND marker.material_id = note_marker.material_id
-                        AND marker.person_id = note_marker.person_id
-    JOIN material ON marker.material_id = material.material_id
-    JOIN lecture ON lecture.material_id = material.material_id
-    ORDER BY person.person_id, note_marker.note_name
-;
+CREATE OR REPLACE VIEW user_notes AS
+    SELECT
+        info."personid",
+        info."notename",
+        LISTAGG(info."substring",
+        '|| ') WITHIN GROUP(
+        ORDER BY
+            info."personid"
+        ) "Notes"
+    FROM
+        (
+            SELECT DISTINCT
+                person.person_id "personid",
+                note_marker.note_name "notename",
+                substr(TO_CHAR(material.material_text),marker.marker_start_point,marker.marker_finish_point - marker.marker_start_point + 1) "substring"
+            FROM
+                person
+                JOIN marker ON person.person_id = marker.person_id
+                JOIN note_marker ON marker.marker_id = note_marker.marker_id
+                                    AND marker.material_id = note_marker.material_id
+                                    AND marker.person_id = note_marker.person_id
+                JOIN material ON marker.material_id = material.material_id
+                JOIN lecture ON lecture.material_id = material.material_id
+        ) info
+    GROUP BY
+        info."personid",
+        info."notename";
