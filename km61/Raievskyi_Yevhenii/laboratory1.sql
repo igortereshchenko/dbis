6bcc60adc05fc8120ed65a0e0d846d91b185ce89
Eugen1344 <Eugen1344@users.noreply.github.com>
2018-05-21 23:41:36 +0300

    mistake fixed

--- km61/Raievskyi_Yevhenii/laboratory1.sql
+++ km61/Raievskyi_Yevhenii/laboratory1.sql
@@ -1,3 +1,26 @@
+/*---------------------------------------------------------------------------
+1.Створити схему даних з назвою – прізвище студента, у якій користувач може: 
+створювати таблиці
+4 бали
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+
+CREATE USER raevskiy IDENTIFIED BY 1344
+DEFAULT TABLESPACE "USER"
+TEMPORARY TABLESPACE "TEMP";
+GRANT 'CONNECT' TO raevskiy;
+GRANT CREATE ANY TABLE TO raevskiy;
+
+
+/*---------------------------------------------------------------------------
+2. Створити таблиці, у яких визначити поля та типи. Головні та зовнішні ключі 
+створювати окремо від таблиць використовуючи команди ALTER TABLE. 
+Студент має мобільний номер іноземного оператора.
+4 бали
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
 CREATE TABLE students
 (
     stud_id NUMBER(15) NOT NULL,
@@ -73,3 +96,92 @@ INSERT INTO students_numbers VALUES (85612, 380675842368, 1);
 INSERT INTO students_numbers VALUES (50, 380671212121, 1);
 INSERT INTO students_numbers VALUES (50, 380673333333, 3);
 INSERT INTO students_numbers VALUES (50, 380674444444, 3);
+
+ 
+/* --------------------------------------------------------------------------- 
+3. Надати додаткові права користувачеві (створеному у пункті № 1) для створення таблиць, 
+внесення даних у таблиці та виконання вибірок використовуючи команду ALTER/GRANT. 
+Згенерувати базу даних використовуючи код з теки OracleScript та виконати запити: 
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+
+GRANT SELECT ANY TABLE to raevskiy;
+GRANT INSERT ANY TABLE to raevskiy;
+
+
+/*---------------------------------------------------------------------------
+3.a. 
+Як звуть покупця, що купив найдешевший товар.
+Виконати завдання в Алгебрі Кодда. 
+4 бали
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+
+PROJECT (DISTINCT(CUST_NAME, PROD_PRICE)
+TIMES
+(CUSTOMERS, ORDERS, ORDERITEMS, PRODUCTS)
+WHERE (CUSTOMERS.CUST_ID = ORDERS.CUST_ID &
+ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM &
+ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID &
+PRODUCTS.PROD_PRICE =
+(PROJECT MIN(PROD_PRICE)
+TIMES
+PRODUCTS
+))
+
+
+/*---------------------------------------------------------------------------
+3.b. 
+Вивести імена покупців, що не мають поштової адреси та замовлення, у дужках - назвавши це поле client_name.
+Виконати завдання в SQL. 
+4 бали
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+
+SELECT DISTINCT
+    '(' || TRIM(cust_name) || ')' AS client_name
+FROM
+    customers
+WHERE
+    cust_email IS NULL
+INTERSECT
+(
+SELECT
+    '(' || TRIM(cust_name) || ')' AS client_name
+FROM
+    customers
+MINUS
+SELECT
+    '(' || TRIM(cust_name) || ')' AS client_name
+FROM
+    customers,
+    orders
+WHERE
+    customers.cust_id = orders.cust_id
+);
+    
+
+/*---------------------------------------------------------------------------
+c. 
+Вивести імена постачальників у верхньому регістрі,назвавши це поле vendor_name, що не мають жодного товару.
+Виконати завдання в SQL. 
+4 бали
+---------------------------------------------------------------------------*/
+--Код відповідь:
+
+SELECT
+    UPPER(vendors.vend_name) as "vendor_name"
+FROM
+    vendors
+MINUS
+SELECT
+    UPPER(vendors.vend_name) as "vendor_name"
+FROM
+    vendors, products
+WHERE
+    vendors.vend_id = products.vend_id;
