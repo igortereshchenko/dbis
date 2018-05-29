181d6f67c54ee347b7de8aec2fe8d6e68a3e711a
Sergiy Babich <Sergbch@gmail.com>
2018-05-29 11:33:18 +0300

    Update laboratory3.sql

--- km61/Babych_Serhii/laboratory3.sql
+++ km61/Babych_Serhii/laboratory3.sql
@@ -1,40 +1,45 @@
--- LABORATORY WORK 3
--- BY Babych_Serhii
-/*1. Ќаписати PL/SQL код, що по вказаному ключу замовленнЯ дода№ в його будь-Який продукт 10 разґв, змґнюючи order_item у циклґ.
-ЋперацґЯ викону№тьсЯ, Якщо у даному замовленнґ вже № хочаб один товар. 10 балґв*/
 
+/*1. Написати PL/SQL код, що дода№ продукт з цґною 10 постачальнику з ключем BRS01, 
+щоб сумарна кылькысть його продуктґв була менша = 10. 
+Ключи нових продуктґв   - prod1iprodn. Решта обовХЯзкових полґв беретьсЯ на вибыр студента.
+10 балґв*/
 
-
-/*2. Ќаписати PL/SQL код, що по вказаному ключу замовленнЯ виводить у консоль його дату та изнача№  його статус.
-џкщо воно ма№ 0 товарґв у серединґ - статус  = "poor"
-џкщо воно ма№ до 2 товарґв включно - статус  = "common"
-џкщо воно ма№ бґльше 2 товарґв - статус  = "rich" 4 бали*/
 DECLARE
-    order_key  ORDERITEMS.ORDER_NUM%TYPE := 'DBS34';
-    order_date CHARACTER;
-    order_status CHARACTER;
-    order_count INTEGER(5);
-
+N_PRODACT_ID VARCHAR(20) := 'prod1iprodn',
+N_PROD_NAME VARCHAR(20) := 'HELLO'
 BEGIN
-    SELECT ORDER_NUM, ORDER_DATE ,SUM(ORDER_ITEMS)
-    INTO order_num, order_date, order_count
-    FROM ORDERITEMS
-    WHERE ORDERITEMS.ORDER_NUM = ORDERS.ORDER_NUM;
-    IF (order_count = 0) THEN
-    order_status := "poor";
-    ELSIF order_count <= 2 THEN
-    order_status := "common";
+SELECT PROD_PRICE VEND_ID
+FROM PRODUCTS
+WHERE PROD_PRICE = 10,00 AND VEND_ID = 'BRS01'
+APPEND PRODUCTS
+(N_PRODACT_ID, VEND_ID, N_PROD_NAME, PROD_PRICE)
+END
+
+/*2. Ќаписати PL/SQL код, що по вказаному ключу замовника виводить у консоль його ґм'Я та визнача№  його статус.
+џкщо вґн купив два рґзних продукти - статус  = "yes"
+џкщо вґн купив бґльше двох рґзних продуктґв- статус  = "no"
+џкщо вґн нема№ замовленнЯ - статус  = "unknown*/
+
+DECLARE
+GET_CUST_ID = &.... /*введення змынної*/
+BEGIN
+    SELECT CUSTOMERS.CUST_NAME CUSTOMERS.CUST_STATE
+    FROM CUSTOMERS
+        JOIN ORDERS AS CUST_iD.CUSTOMERS = CUST_ID.ORDERS
+        INSERT JOIN ORDRITEMS SA ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+    WHERE CUST_ID = GET_CUST_ID
+    IF (SELECT ORDEITEMS.PROD_ID FROM ORDEITEMS WHERE count(ORDERITEMS.PROD_ID) == 2)
+        DBMS_OUTPUB.OUTLINE("Name: "|| CUSTOMERS.CUST_NAME ||",Status: yes")
+    ELSIF (SELECT ORDEITEMS.PROD_ID FROM ORDEITEMS WHERE count(ORDERITEMS.PROD_ID) >= 2)
+         DBMS_OUTPUB.OUTLINE("Name: "||CUSTOMERS.CUST_NAME||",Status: no")
     ELSE
-    order_status := "rich";
-    END IF;
+         DBMS_OUTPUB.OUTLINE("Name: "||CUSTOMERS.CUST_NAME||",Status: unknown")
+    END IF
+END
 
-END;
+/*3. Створити представленнЯ та використати його у двох запитах:
+3.1. Скыльки товарыв було куплено покупцЯми з Ђнглґ».
+3.2. Ќа Яку загальну суму купили покупцґ товари з Ђнглґ».
+6 балґв.*/
 
-/*3. ‘творити предсавленнЯ та використати його у двох запитах:
-3.1. ‚ивести ключ постачальника та номер замовленнЯ, куди прода№ постачальник сво» продукти.
-3.2. ‚ивести ґм'Я постачальника та загальну кґлькґсть проданих ним товарґв 6 балґв.*/
-CREATE VIEW MY_VIEW AS
-   SELECT VEND_NAME
-   FROM VENDORS;
-   
-   SELECT
+
