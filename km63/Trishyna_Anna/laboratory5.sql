5406e76460be427726b1b03fd18ea15f7d3782a1
TrishynaAnna <37344231+TrishynaAnna@users.noreply.github.com>
2018-06-02 15:24:51 +0300

    Update laboratory5.sql

--- km63/Trishyna_Anna/laboratory5.sql
+++ km63/Trishyna_Anna/laboratory5.sql
@@ -1,2 +1,34 @@
 -- LABORATORY WORK 5
 -- BY Trishyna_Anna
+CREATE OR REPLACE FUNCTION prod_id (in cust_id number)
+RETURN number
+IS
+prod_id  number;
+BEGIN 
+SELECT COUNT(PROD_ID) INTO PROD_COUNT FROM PRODUCT WHERE VEND_ID=VENDOR_ID;
+RETURN PROD_ID;
+END
+
+
+CREATE PROCEDURE proc(new_name products.prod_id%Type)
+BEGIN
+SELECT prod_id into old_name 
+FROM products
+WHERE prod_id=new_name;
+IF (count(prod_id)=1) and filter is not NULL
+THEN UPDATE PRODUCTS
+SET PRODUCTS prod_id=
+
+CREATE PROCEDURE get_vend_id (in vend_name varchar, out vendor_id number)
+BEGIN
+SELECT NVL(VEND_ID,0)
+FROM VENDORS 
+WHERE
+VEND_NAME=vend_name;
+IF vendor_id==0
+THEN 
+END IF
+END
+
+
+
