ee84600c629ba8429f7a9e452185502bd1c70ece
Dmitriy Koltsov <dik_2010@ukr.net>
2018-06-02 14:26:30 +0200

    Update laboratory5.sql

--- km63/Koltsov_Dmytro/laboratory5.sql
+++ km63/Koltsov_Dmytro/laboratory5.sql
@@ -1,2 +1,31 @@
 -- LABORATORY WORK 5
 -- BY Koltsov_Dmytro
+create or replace FUNCTION coun_null(vend VENDORS.vend_id%type)
+RETURN INT
+AS
+resul INT;
+begin
+select
+count(*) into resul
+from
+VENDORS left join PRODUCTS on
+VENDORS.vend_id = PRODUCTS.vend_id
+left join ORDERITEMS on
+PRODUCTS.PROD_ID = ORDERITEMS.PROD_ID
+where VENDORS.vend_id = vend and order_num is null;
+
+return resul;
+end coun_null;
+
+create or replace PROCEDURE PR_NAME (prod_name in PRODUCTS.PROD_NAME%type, keye out ORDERITEMS.ORDER_NUM%type)
+IS
+BEGIN
+
+select
+order_num into keye
+from
+PRODUCTS left join ORDERITEMS on
+PRODUCTS.prod_id = ORDERITEMS.PROD_ID
+where
+PRODUCTS.PROD_NAME = prod_name;
+END PR_NAME;
