4734bc607e67a49fb5161b156d4d9936e615c9cf
VadimPits <34885873+VadimPits@users.noreply.github.com>
2018-06-02 14:55:28 +0300

    Update laboratory5.sql

--- km63/Pits_Vadym/laboratory5.sql
+++ km63/Pits_Vadym/laboratory5.sql
@@ -1,2 +1,16 @@
--- LABORATORY WORK 5
--- BY Pits_Vadym
+create function null_order
+
+select orders.order_num 
+from orders join customers
+on cutomers.cust_id = orders.cust_id
+where orders.order_num is null
+
+create procedure prod_value
+in 
+declare prod_price
+
+create view view1
+select products.prod_name from products
+
+create view view2
+select products.prod_id from products
