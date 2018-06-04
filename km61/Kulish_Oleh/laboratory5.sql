6596deb6afdf9744ddb6e16ba61f6b3d03dd23d7
OlehKulysh <olehavdiyivka@gmail.com>
2018-06-04 15:13:07 +0300

    Update laboratory5.sql

--- km61/Kulish_Oleh/laboratory5.sql
+++ km61/Kulish_Oleh/laboratory5.sql
@@ -1,2 +1,21 @@
 -- LABORATORY WORK 5
 -- BY Kulish_Oleh
+create or replace function foo id1 
+  return int, string
+  is 
+  
+  declare
+  begin 
+  end
+  return select cust_id, cust_name from customers
+    where customers.cust_id = id1s;
+
+
+
+
+create or replace procedure foo1 nume_cust 
+  is 
+  declare
+  begin 
+    select 
+  end
