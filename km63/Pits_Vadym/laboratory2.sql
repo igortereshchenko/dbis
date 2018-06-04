b759a5c48aa835ebe3e5e992114cf05f05c805a7
VadimPits <34885873+VadimPits@users.noreply.github.com>
2018-06-04 15:47:44 +0300

    Update laboratory2.sql

--- km63/Pits_Vadym/laboratory2.sql
+++ km63/Pits_Vadym/laboratory2.sql
@@ -1,2 +1,40 @@
--- LABORATORY WORK 2
--- BY Pits_Vadym
+/*---------------------------------------------------------------------------
+1. Вивести країну постачальника, що продав менше 2 різних продуктів двом різним покупцям.
+-------------------------------------------------------------------------*/
+--Код відповідь:
+
+select PRODUCTS.PROD_NAME,
+       CUSTOMERS.CUST_NAME,
+       VENDORS.VEND_COUNTRY
+       
+from   VENDORS join PRODUCTS
+on     VENDORS.VEND_ID = PRODUCTS.VEND_ID
+       join ORDERITEMS 
+       on PRODUCTS.PROD_ID = ORDERITEMS.PROD_ID
+       join ORDERS
+       on ORDERITEMS.ORDER_NUM = ORDERS.ORDER_NUM
+       join CUSTOMERS
+       on ORDERS.CUST_ID = CUSTOMERS.CUST_ID
+       
+group by VENDORS.VEND_COUNTRY,CUSTOMERS.CUST_NAME
+having count (distinct PRODUCTS.PROD_NAME)<2,(distinct CUSTOMERS.CUST_NAME)=2 
+
+
+/*---------------------------------------------------------------------------
+2.  Вивести ключ покупця, що має замовлення, що містить найдорожчий продукт.
+---------------------------------------------------------------------------*/
+
+--Код відповідь:
+
+select CUSTOMERS.CUST_ID
+       max(PRODUCTS.PROD_PRICE)
+       
+from CUSTOMERS join ORDERS
+on CUSTOMERS.CUST_ID = ORDERS.CUST_ID
+join ORDERITEMS
+on ORDERS.ORDER_NUM = ORDERITEMS.ORDER_NUM
+join PRODUCTS
+on ORDERITEMS.PROD_ID = PRODUCTS.PROD_ID
+
+group by CUSTOMERS.CUST_ID,PRODUCTS.PROD_PRICE
+where (distinct CUSTOMERS.CUST_ID) = max(PRODUCTS.PROD_PRICE)
