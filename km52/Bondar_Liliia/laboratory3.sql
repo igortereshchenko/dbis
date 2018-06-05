1ec6ea35540ca5208a2ca829b5ae6b8da2ca8b1d
Лилия Бондарь <knopochka.135@gmail.com>
2018-06-05 11:00:13 +0300

    Update laboratory3.sql

--- km52/Bondar_Liliia/laboratory3.sql
+++ km52/Bondar_Liliia/laboratory3.sql
@@ -1,84 +1,104 @@
 -- LABORATORY WORK 3
 -- BY Bondar_Liliia
-/*1. Написати PL/SQL код, що додає замовників, щоб сумарна кількість усіх замовників була 10. Ключі замовників test1….testn. 
+/*1. Написати PL/SQL код, що додає замовників, щоб сумарна кількість усіх
+замовників була 10. Ключі замовників test1….testn.
 Решта значень обов’язкових полів відповідає полям  замовника з ключем 1000000001.
 10 балів*/
-Declare 
-  add_count integer := 10;
-  
-begin
+SET SERVEROUTPUT ON
+DECLARE
+  vendors_count INTEGER;
+BEGIN
+  LOOP
+    SELECT COUNT(*) INTO vendors_count FROM vendors;
+    DBMS_OUTPUT.put_line('Vendors count: ' || vendors_count);
+    EXIT
+  WHEN vendors_count = 10;
+    INSERT
+    INTO vendors
+      (
+        vend_id,
+        vend_name
+      )
+      VALUES
+      (
+        'test' || i,
+        '1000000001'
+      );
+  END LOOP;
+END;
 
-end;
-
-
-/*2. Написати PL/SQL код, що по вказаному ключу постачальника виводить у консоль його ім'я та визначає  його статус.
-Якщо він продав найдешевший продукт - статус  = "yes"
-Якщо він продав не продавав найдешевший - статус  = "no"
-Якщо він не продавав жодного продукту - статус  = "unknown*/
-declare 
-    wanted_vendor_id vendors.vend_id%TYPE  &= wanted_vendor_id;
-    min_price := select min(products.prod_price)
-                  from products left join vendors on
-                  products.vend_id = vendors.vend_id;
-    wanted_vendor_record := select * 
-                            from products left join vendors on
-                            products.vend_id = vendors.vend_id
-                            where products.vend_id = wanted_vendor_id;
-begin
-  if wanted_vendor_record.prod_price = min_price
-end;
-
-
+/*2. Написати PL/SQL код, що по вказаному ключу замовника виводить у консоль
+його ім'я та визначає  його статус.
+Якщо він має більше 3 замовлень - статус  = "yes"
+Якщо він має не менше 3 замовлень - статус  = "no"
+Якщо він немає замовлення - статус  = "unknown*/
+DECLARE
+  vend_id vendors.vend_id%TYPE := 'BRS01';
+  vend_name vendors.vend_name%TYPE;
+  orders_count INTEGER;
+  status       VARCHAR2(8);
+BEGIN
+  SELECT vendors.vend_name,
+    COUNT(DISTINCT orders.order_num)
+  INTO vend_name,
+    orders_count
+  FROM vendors
+  JOIN products
+  ON vendors.vend_id = products.vend_id
+  JOIN orderitems
+  ON products.prod_id = orderitems.prod_id
+  JOIN orders
+  ON orders.order_num   = orderitems.order_num
+  WHERE vendors.vend_id = 'BRS01'
+  GROUP BY vendors.vend_name;
+  IF orders_count    > 3 THEN
+    status          := 'yes';
+  ELSIF orders_count > 0 THEN
+    status          := 'no';
+  ELSE
+    status := 'unknown';
+  END IF;
+  DBMS_OUTPUT.put_line('Vendor ' || TRIM(vend_name) || ' status is ' || status);
+END;
+SELECT vendors.vend_name
+FROM vendors
+JOIN products
+ON vendors.vend_id = products.vend_id
+JOIN orderitems
+ON products.prod_id = orderitems.prod_id
+JOIN orders
+ON orders.order_num   = orderitems.order_num
+WHERE vendors.vend_id = 'BRS01';
 
 /*3. Створити представлення та використати його у двох запитах:
-3.1. Яку сумарну кількість товарів продали постачальники, що проживають в Германії.
-3.2. Вивести ім’я покупця та загальну кількість купленим ним товарів.
+3.1. Скільки замовлень має кожен з покупців, що проживає в Австрії.
+3.2. Як звуть постачальника з Германії, що продає свої товари більше ніж у 3 різних замовленнях.
 6 балів.*/
-
-CREATE VIEW ALIB AS
-SELECT customers.CUST_ID CUST_ID,
-customers.CUST_NAME CUST_NAME,
-customers.CUST_ADDRESS CUST_ADDRESS,
-customers.CUST_CITY CUST_CITY,
-customers.CUST_STATE CUST_STATE,
-customers.CUST_ZIP CUST_ZIP,
-customers.CUST_COUNTRY CUST_COUNTRY,
-customers.CUST_CONTACT CUST_CONTACT,
-customers.CUST_EMAIL CUST_EMAIL,
-orders.ORDER_NUM ORDER_NUM,
-orders.ORDER_DATE ORDER_DATE,
-orders.CUST_ID CUST_ID_0,
-orderitems.ORDER_NUM ORDER_NUM_1,
-orderitems.ORDER_ITEM ORDER_ITEM,
-orderitems.PROD_ID PROD_ID,
-orderitems.QUANTITY QUANTITY,
-orderitems.ITEM_PRICE ITEM_PRICE,
-products.PROD_ID PROD_ID_2,
-products.VEND_ID VEND_ID,
-products.PROD_NAME PROD_NAME,
-products.PROD_PRICE PROD_PRICE,
-products.PROD_DESC PROD_DESC,
-vendors.VEND_ID VEND_ID_3,
-vendors.VEND_NAME VEND_NAME,
-vendors.VEND_ADDRESS VEND_ADDRESS,
-vendors.VEND_CITY VEND_CITY,
-vendors.VEND_STATE VEND_STATE,
-vendors.VEND_ZIP VEND_ZIP,
-vendors.VEND_COUNTRY VEND_COUNTRY 
+CREATE VIEW customers_orders_vendors AS
+SELECT customers.cust_id,
+  customers.cust_country,
+  orders.order_num,
+  vendors.vend_id,
+  vendors.vend_name,
+  vendors.vend_country
 FROM customers
-inner JOIN orders
+JOIN orders
 ON customers.cust_id = orders.cust_id
-inner JOIN orderitems
+JOIN orderitems
 ON orders.order_num = orderitems.order_num
-inner JOIN products
-ON orderitems.prod_id = products.prod_id
-inner JOIN vendors
-ON products.vend_id = vendors.vend_id; 
-
-select sum(count(alib.prod_id))
-from alib
-where alib.vend_country = 'Germany'
-group by (ALIB.vend_name);
-select alib.cust_name, sum(alib.quantity), alib.cust_id
-from alib
-group by (alib.cust_name, alib.cust_id);
+JOIN products
+ON products.prod_id = orderitems.prod_id
+JOIN vendors
+ON vendors.vend_id = products.vend_id;
+SELECT cust_id,
+  COUNT(*)
+FROM customers_orders_vendors
+WHERE cust_country = 'Australia'
+GROUP BY cust_id;
+SELECT vend_id,
+  vend_name,
+  COUNT(*)
+FROM customers_orders_vendors
+GROUP BY vend_id,
+  vend_name
+HAVING COUNT(DISTINCT order_num) > 3;
