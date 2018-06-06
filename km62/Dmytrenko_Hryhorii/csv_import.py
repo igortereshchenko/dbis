622ac22e79f1a64a3b63c65ce70bcbfb6b4dc87f
Григорий <loco_inverso@ukr.net>
2018-06-06 11:56:55 +0300

    Update csv_import.py

--- km62/Dmytrenko_Hryhorii/csv_import.py
+++ km62/Dmytrenko_Hryhorii/csv_import.py
@@ -7,7 +7,7 @@ filename = "human_100015.csv"
 with open(filename, newline='') as file:
     reader = csv.reader(file)
 
-    id = next(reader)[1]
+    h_id = next(reader)[1]
     name = next(reader)[1]
     sex = next(reader)[1]
     age = next(reader)[1]
@@ -15,14 +15,11 @@ with open(filename, newline='') as file:
 
     insert_query = "insert into HUMAN(human_id, human_name, human_sex, human_age, human_country) values (:human_id, :human_name, :human_sex, :human_age, :human_country)"
     cursor_human = connection.cursor()
-    cursor_human.execute(insert_query, human_id = id, human_name = name, human_sex = sex, human_age = age, human_country = country)
+    cursor_human.execute(insert_query, human_id = h_id, human_name = name, human_sex = sex, human_age = age, human_country = country)
     cursor_human.close()
-
-    connection.commit() 
-
-    next(reader, None) 
-    next(reader, None) 
-
+    connection.commit()
+    next(reader, None)
+    next(reader, None)
     insert_query = "INSERT INTO PHONE_TYPE (phone_id, phone_type_name, human_id) VALUES (:phone_id, :phone_type_name, :human_id)"
     cursor_phone_type = connection.cursor()
 
@@ -30,7 +27,7 @@ with open(filename, newline='') as file:
 
     rows = []
     for row in reader:
-        rows.append(row+[id])
+        rows.append(row+[h_id])
 
     cursor_phone_type.executemany(None, rows)
 
