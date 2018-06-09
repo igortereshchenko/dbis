59360f77efbc496387bf6e9a42718c88700275aa
YaroslavArtemenko <Yaroslavhik@yandex.ua>
2018-06-09 14:01:58 +0300

    Update studentpma~queries.sql

--- km63/Artemenko_Yaroslav/studentpma~queries.sql
+++ km63/Artemenko_Yaroslav/studentpma~queries.sql
@@ -1,17 +1,38 @@
-create view stud_answer as
-select DISTINCT student.student_id, exercise_answer_id, DBMS_LOB.substr(exercise_solution, 1000), max(exercise_mark), exercise_mark
-from student join exercise_answer on student.student_id = exercise_answer.student_id
-             join exercise on exercise_answer.exercise_id = exercise.exercise_id
-group by student.student_id, exercise_answer_id, exercise_solution, exercise_mark
-order by student.student_id, exercise_answer.exercise_answer_id;
+create view exercise_comment as 
+select DISTINCT exercise.exercise_number, comments.comment_id
+from exercise join exercise_answer on exercise.exercise_number = exercise_answer.exercise_number
+              join student on exercise_answer.student_id = student.student_id
+              join person on student.person_id = person.person_id
+              join marker on person.person_id = marker.person_id
+              join comments on marker.marker_id = comments.marker_id
+group by exercise.exercise_number, comments.comment_id
+order by exercise.exercise_number;
 
 
 
-select student.student_id, EXERCISE_ANSWER.STUDENT_SOLUTION_MARK
-from student join exercise_answer on student.student_id = exercise_answer.student_id
-group by student.STUDENT_ID, EXERCISE_ANSWER.STUDENT_SOLUTION_MARK;
+select exercise.exercise_number , count(comments.comment_id)
+from exercise join exercise_answer on exercise.exercise_number = exercise_answer.exercise_number
+              join student on exercise_answer.student_id = student.student_id
+              join person on student.person_id = person.person_id
+              join marker on person.person_id = marker.person_id
+              join comments on marker.marker_id = comments.marker_id
+group by exercise.exercise_number;   
 
 
-select exercise.exercise_number, material.material_id
-from material join exercise on material.material_id = exercise.material_id
-group by exercise.exercise_number, material.material_id;
+
+
+select person.person_id, count(comments.comment_id)
+from person join marker on person.person_id = marker.person_id
+            join comments on marker.marker_id = comments.comment_id
+group by person.person_id;
+
+
+
+
+select person.person_id, count(comments.comment_id), exercise.exercise_number
+from exercise join exercise_answer on exercise.exercise_number = exercise_answer.exercise_number
+              join student on exercise_answer.student_id = student.student_id
+              join person on student.person_id = person.person_id
+              join marker on person.person_id = marker.person_id
+              join comments on marker.marker_id = comments.marker_id
+group by person.person_id, exercise.exercise_number;
