b083c76087205b93ac49015f5ebfbf5f4bd09d1f
Samovilov <sergik241997@gmail.com>
2018-06-09 10:17:28 +0300

    Update laboratory4.sql

--- km63/Samovilov_Serhii/laboratory4.sql
+++ km63/Samovilov_Serhii/laboratory4.sql
@@ -1,2 +1,34 @@
 -- LABORATORY WORK 4
+CREATE or REPLACE TRIGGER delete_news
+ BEFORE 
+ UPDATE OF fact_date
+ ON fb_facts
+ BEGIN 
+ DELETE FROM fb_news 
+ END; 
+
+
+CREATE or REPLACE TRIGGER news_for_user
+before INSERT ON fb_users
+DECLARE
+new_fact_name facts.fact_name%TYPE, 
+new_news_id fb_news.news_id%TYPE
+BEGIN 
+INSERT INTO user read news
+Values(new.user_id, new_fact_name, new_news_id);
+END;
+
+
+  DECLARE
+   CURSOR fact_cursor (v_fact_name facts.fact_name%TYPE)
+   IS 
+   SELECT 
+   facts.fact_date,
+   user_read_news.user_id
+   where fact_name=v_fact_name;
+   
+   
+   
+   
+
 -- BY Samovilov_Serhii
